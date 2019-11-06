1a157064678ae2e48dc52479b19b6d8f48d15c7b
Andres Ruiz <andrsruiz@gmail.com>
2019-11-05 22:49:08 -0600

    Add Functions
    New entry and print database functions

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,13 @@
 // Function declaration
 void displayWelcomeMessage();
 void displayMenuOptions();
+std::vector<Owner> makeOwnerEntry(std::vector<Owner>);
+void printDataBase(std::vector<Owner>);
 
 int main(){
 	bool menu = true;
 	char option;
+	std::vector<Owner> ownerVector;
 	
 	displayWelcomeMessage();
 	while(menu){
@@ -28,8 +31,10 @@ int main(){
 				menu = false;
 				break;
 			case 'A' :
+				ownerVector = makeOwnerEntry(ownerVector);
 				break;
 			case 'B' :
+				printDataBase(ownerVector);
 				break;
 			default :
 				break;
@@ -54,4 +59,38 @@ void displayMenuOptions(){
 	std::cout << "A : New entry.\n";
 	std::cout << "B : Print database.\n";
 	std::cout << "q : Quit.\n";
+}
+
+std::vector<Owner> makeOwnerEntry(std::vector<Owner> myOwnerVector){
+	std::string ownerName = "", ownerLastName = "";
+	std::string ownerEmail = "", ownerAddress = "";
+	std::string ownerPhoneNumber = "";
+
+	std::cout << "Enter owner information\n";
+	std::cout << "Name : ";
+	std::cin.ignore();
+	std::getline(std::cin, ownerName);
+	std::cout << "Last Name : ";
+	std::cin.ignore();
+	std::getline(std::cin, ownerLastName);
+	std::cout << "Email : ";
+	std::cin >> ownerEmail;
+	std::cout << "Address : ";
+	std::cin.ignore();
+	std::getline(std::cin, ownerAddress);
+	std::cout << "Phone Number : ";
+	std::cin >> ownerPhoneNumber;
+
+	Owner myOwner(ownerName, ownerLastName, ownerEmail, ownerAddress, ownerPhoneNumber);
+	myOwnerVector.push_back(myOwner);
+
+	return myOwnerVector;
+}
+
+void printDataBase(std::vector<Owner> myOwnerVector){
+	for(int i = 0; i < myOwnerVector.size(); i++){
+		std::cout << myOwnerVector[i].getName() << " " << myOwnerVector[i].getLastName() << std::endl;
+		std::cout << myOwnerVector[i].getAddress() << std::endl;
+		std::cout << myOwnerVector[i].getPhoneNumber() << std::endl;
+	}
 }
