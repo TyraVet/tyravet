763524061fcf976b84db07853678684ea75f73b4
Andres Ruiz <andrsruiz@gmail.com>
2020-04-02 14:34:25 -0600

    Test Client class

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include "../include/Owner.hpp"
+#include "../include/Client.hpp"
 #include <gtkmm/application.h>
 #include "../include/helloworld.hpp"
 
@@ -14,12 +15,19 @@ int main(int argc, char *argv[]){
 
     Owner me = Owner("andres", "ruiz", "male", "andres@mail.com", "micasa #90", "93405900");
 
-    std::cout << me.getName() << "\n";
-    std::cout << me.getLastName() << "\n";
-    std::cout << me.getGender() << "\n";
-    std::cout << me.getEmail() << "\n";
-    std::cout << me.getAddress() << "\n";
-    std::cout << me.getPhoneNumber() << "\n";
+    // std::cout << me.getName() << "\n";
+    // std::cout << me.getLastName() << "\n";
+    // std::cout << me.getGender() << "\n";
+    // std::cout << me.getEmail() << "\n";
+    // std::cout << me.getAddress() << "\n";
+    // std::cout << me.getPhoneNumber() << "\n";
+
+    Pet coque = Pet();
+    Client meAgain = Client(me, coque);
+
+    std::cout << meAgain.getOwner().getName() << "\n";
+    std::cout << meAgain.getPet().getBreed() << "\n";
+
 
     return app->run(helloworld);
 }
