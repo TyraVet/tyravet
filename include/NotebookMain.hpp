8e57d4e4b1ae14198f2da20722281e4f3eb86a16
Andres Ruiz <andrsruiz@gmail.com>
2020-04-06 13:39:28 -0500

    Update Notebook
    At the moment the calendar is not working

--- include/NotebookMain.hpp
+++ include/NotebookMain.hpp
@@ -3,15 +3,19 @@
 #include <gtkmm.h>
 
 class NotebookMain : public Gtk::Window{
-public:
-    NotebookMain();
-    virtual ~NotebookMain();
+    public:
+        NotebookMain();
+        virtual ~NotebookMain();
 
-protected:
-    // Signal handlers
-    void on_notebook_switch_page(Gtk::Widget *page, guint page_num);
+    protected:
+        // Signal handlers
+        void on_notebook_switch_page(Gtk::Widget *page, guint page_num);
+        void on_button_add_appointment();
 
-    // Member widgets
-    Gtk::Notebook notebookMain;
-    Gtk::Label labelDatebook, labelHospital, labelInventory;
+        // Member widgets
+        Gtk::Notebook notebookMain;
+        Gtk::Label labelHospital, labelInventory, labelTest;
+        Gtk::Box boxDatebook, boxCalendar, boxAppointments;
+        Gtk::Calendar calendar;
+        Gtk::Button buttonAddAppointment;
 };
--- src/NotebookMain.cpp
+++ src/NotebookMain.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
+#include <string>
 #include "../include/NotebookMain.hpp"
 
-NotebookMain::NotebookMain() : labelDatebook("Hello from inside Datebook!"),
-                           labelHospital("Hello from inside Hospital!"),
-                           labelInventory("Hello from inside Inventory!"){
-    set_title("Tyra");
-    set_default_size(640, 480);
+NotebookMain::NotebookMain() : labelHospital("Hello from inside Hospital!"),
+                               labelInventory("Hello from inside Inventory!"),
+                               labelTest("Hello from inside Appointments"){
+    this->set_title("Tyra");
+    this->set_default_size(640, 480);
+
+    // Create Calendar Box
+    boxCalendar.set_orientation(Gtk::ORIENTATION_VERTICAL);
+    boxCalendar.pack_start(calendar);
+    buttonAddAppointment.set_label("Add Appointment");
+    boxCalendar.pack_start(buttonAddAppointment);
+    // Create Appointments Box
+    boxAppointments.set_orientation(Gtk::ORIENTATION_VERTICAL);
+    boxAppointments.pack_start(labelTest);
+    // Create Datebok Box
+    boxDatebook.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
+    boxDatebook.pack_start(boxCalendar);
+    boxDatebook.pack_end(boxAppointments);
 
     // Set Notebook
     notebookMain.set_border_width(0);
     // Add Notebook tabs
-    notebookMain.append_page(labelDatebook, "Datebook");
+    notebookMain.append_page(boxDatebook, "Datebook");
     notebookMain.append_page(labelHospital, "Hospital");
     notebookMain.append_page(labelInventory, "Inventory");
-    // Switch tabs
-    notebookMain.signal_switch_page().connect(sigc::mem_fun(*this,
-                                                            &NotebookMain::on_notebook_switch_page));
 
-    add(notebookMain);
-    show_all_children();
+    // Switch tabs
+    notebookMain.signal_switch_page().connect(
+        sigc::mem_fun(*this, &NotebookMain::on_notebook_switch_page));
+    // Get Date
+    buttonAddAppointment.signal_clicked().connect(
+        sigc::mem_fun(*this, &NotebookMain::on_button_add_appointment));
+
+    // Add Main Window (Notebook)
+    this->add(notebookMain);
+    this->show_all_children();
 }
 
 NotebookMain::~NotebookMain(){}
 
 void NotebookMain::on_notebook_switch_page(Gtk::Widget * /* page */, guint page_num){
     std::cout << "Switched to tab with index : " << page_num << std::endl;
+}
+
+void NotebookMain::on_button_add_appointment(){
+    std::cout << "Button clicked\n";
+    // std::cout << calendar.get_date() << "\n"; This doesn't work!
 }
