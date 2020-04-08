7142cb8f23cd37d2c0cacdc1046a15e3d174be58
Andres Ruiz <andrsruiz@gmail.com>
2020-04-08 13:53:17 -0500

    Update main
    No need for outputs here

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 
 /* TYRA by Andres Ruiz */
 
-#include <iostream>
-#include "../include/Owner.hpp"
-#include "../include/Pet.hpp"
 #include <gtkmm/application.h>
 #include "../include/NotebookMain.hpp"
 
@@ -12,15 +9,6 @@ int main(int argc, char *argv[]){
     auto app = Gtk::Application::create(argc, argv, "dev.tyra");
 
     NotebookMain notebookMain;
-
-    // Owner me = Owner("andres", "ruiz", "male", "andres@mail.com", "micasa #90", "93405900");
-
-    // std::cout << me.getName() << "\n";
-    // std::cout << me.getLastName() << "\n";
-    // std::cout << me.getGender() << "\n";
-    // std::cout << me.getEmail() << "\n";
-    // std::cout << me.getAddress() << "\n";
-    // std::cout << me.getPhoneNumber() << "\n";
 
     return app->run(notebookMain);
 }
