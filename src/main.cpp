5cb57f4ea5bb4a473e9fb26e9bb098f6eff33f94
Andres Ruiz <andrsruiz@gmail.com>
2020-04-16 15:45:04 -0500

    Update main
    Added terminal menu
    Option to run gui or not

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,77 @@
 
 /* TYRA by Andres Ruiz */
 
+#include <iostream>
+#include <string>
 #include <gtkmm-3.0/gtkmm.h>
-#include "../include/NotebookMain.hpp"
+#include "../include/NotebookMain.hpp" /* GUI */
 
-int main(int argc, char *argv[]){
-    auto app = Gtk::Application::create(argc, argv, "dev.tyra");
+const char a = 'a';
+const char c = 'c';
+const char q = 'q';
+const char t = 't';
+const std::string WEB_PAGE = "https://github.com/Andrsrz";
 
-    NotebookMain notebookMain;
+/* Function declaration */
+void printWelcomeMessage();
+void printOptions();
+void printAbout();
 
-    return app->run(notebookMain);
+int main(int argc, char* argv[]){
+    /* GUI */
+    if(argc > 1){
+        /*
+         * I needed to create an aditional string to compare to.
+         * It tourns out that a char* and a string can't be compared.
+         */
+        std::string arg1(argv[1]);
+        if(arg1 == "-gui"){
+            auto app = Gtk::Application::create("dev.tyra");
+            NotebookMain notebookMain;
+            return app->run(notebookMain);
+        }
+    }
+
+    /* MENU */
+    char option;
+    bool quit = false;
+    printWelcomeMessage();
+    while(!quit){
+        printOptions();
+        std::cin >> option;
+        switch(option){
+            case ::a:
+                break;
+            case ::c:
+                break;
+            case ::q:
+                std::cout << "Exiting program...\n";
+                quit = true;
+                break;
+            case ::t:
+                printAbout();
+                break;
+            default:
+                std::cout << "Enter a valid option!\n\n";
+                break;
+        }
+    }
+
+    return 0;
+}
+
+/* Function implementation */
+void printWelcomeMessage(){ std::cout << "\tWelcome to Tyra!\n"; }
+
+void printOptions(){
+    std::cout << "\ta - Add new Pet\n";
+    std::cout << "\tc - Check DataBase\n";
+    std::cout << "\tq - Exit the program\n";
+    std::cout << "\tt - About\n\n";
+}
+
+void printAbout(){
+    std::cout << "\tTyra. Veterinary Management.\n";
+    std::cout << "\tBy Andres Ruiz\n";
+    std::cout << "\t" << ::WEB_PAGE << "\n";
 }
