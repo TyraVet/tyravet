13a982baae1be13167a684d78a6305ed54995dc1
Andres Ruiz <andrsruiz@gmail.com>
2020-04-16 15:44:41 -0500

    Redo Comments
    Better comments

--- src/NotebookMain.cpp
+++ src/NotebookMain.cpp
@@ -9,34 +9,34 @@ NotebookMain::NotebookMain() : labelHospital("Hello from inside Hospital!"),
     this->set_title("Tyra");
     this->set_default_size(640, 480);
 
-    // Create Calendar Box
+    /* Create Calendar Box */
     boxCalendar.set_orientation(Gtk::ORIENTATION_VERTICAL);
     boxCalendar.pack_start(calendar);
     buttonAddAppointment.set_label("Add Appointment");
     boxCalendar.pack_start(buttonAddAppointment);
-    // Create Appointments Box
+    /* Create Appointments Box */
     boxAppointments.set_orientation(Gtk::ORIENTATION_VERTICAL);
     boxAppointments.pack_start(labelTest);
-    // Create Datebok Box
+    /* Create Datebok Box */
     boxDatebook.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
     boxDatebook.pack_start(boxCalendar);
     boxDatebook.pack_end(boxAppointments);
 
-    // Set Notebook
+    /* Set Notebook */
     notebookMain.set_border_width(0);
-    // Add Notebook tabs
+    /* Add Notebook tabs */
     notebookMain.append_page(boxDatebook, "Datebook");
     notebookMain.append_page(labelHospital, "Hospital");
     notebookMain.append_page(labelInventory, "Inventory");
 
-    // Switch tabs
+    /* Switch tabs */
     notebookMain.signal_switch_page().connect(
         sigc::mem_fun(*this, &NotebookMain::on_notebook_switch_page));
-    // Get Date
+    /* Get Date */
     buttonAddAppointment.signal_clicked().connect(
         sigc::mem_fun(*this, &NotebookMain::on_button_add_appointment));
 
-    // Add Main Window (Notebook)
+    /* Add Main Window (Notebook) */
     this->add(notebookMain);
     this->show_all_children();
 }
