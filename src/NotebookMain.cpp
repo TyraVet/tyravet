7cc0e6221dea91b849cfd6708896db04fa0f40db
Andres Ruiz <andrsruiz@gmail.com>
2020-04-20 13:20:20 -0500

    Change to TABS

--- src/NotebookMain.cpp
+++ src/NotebookMain.cpp
@@ -4,52 +4,52 @@
 #include "../include/AddPet.hpp"
 
 NotebookMain::NotebookMain() : labelHospital("Hello from inside Hospital!"),
-                               labelInventory("Hello from inside Inventory!"),
-                               labelTest("Hello from inside Appointments"){
-    this->set_title("Tyra");
-    this->set_default_size(640, 480);
+								labelInventory("Hello from inside Inventory!"),
+								labelTest("Hello from inside Appointments"){
+	this->set_title("Tyra");
+	this->set_default_size(640, 480);
 
-    /* Create Calendar Box */
-    boxCalendar.set_orientation(Gtk::ORIENTATION_VERTICAL);
-    boxCalendar.pack_start(calendar);
-    buttonAddAppointment.set_label("Add Appointment");
-    boxCalendar.pack_start(buttonAddAppointment);
-    /* Create Appointments Box */
-    boxAppointments.set_orientation(Gtk::ORIENTATION_VERTICAL);
-    boxAppointments.pack_start(labelTest);
-    /* Create Datebok Box */
-    boxDatebook.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
-    boxDatebook.pack_start(boxCalendar);
-    boxDatebook.pack_end(boxAppointments);
+	/* Create Calendar Box */
+	boxCalendar.set_orientation(Gtk::ORIENTATION_VERTICAL);
+	boxCalendar.pack_start(calendar);
+	buttonAddAppointment.set_label("Add Appointment");
+	boxCalendar.pack_start(buttonAddAppointment);
+	/* Create Appointments Box */
+	boxAppointments.set_orientation(Gtk::ORIENTATION_VERTICAL);
+	boxAppointments.pack_start(labelTest);
+	/* Create Datebok Box */
+	boxDatebook.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
+	boxDatebook.pack_start(boxCalendar);
+	boxDatebook.pack_end(boxAppointments);
 
-    /* Set Notebook */
-    notebookMain.set_border_width(0);
-    /* Add Notebook tabs */
-    notebookMain.append_page(boxDatebook, "Datebook");
-    notebookMain.append_page(labelHospital, "Hospital");
-    notebookMain.append_page(labelInventory, "Inventory");
+	/* Set Notebook */
+	notebookMain.set_border_width(0);
+	/* Add Notebook tabs */
+	notebookMain.append_page(boxDatebook, "Datebook");
+	notebookMain.append_page(labelHospital, "Hospital");
+	notebookMain.append_page(labelInventory, "Inventory");
 
-    /* Switch tabs */
-    notebookMain.signal_switch_page().connect(
-        sigc::mem_fun(*this, &NotebookMain::on_notebook_switch_page));
-    /* Get Date */
-    buttonAddAppointment.signal_clicked().connect(
-        sigc::mem_fun(*this, &NotebookMain::on_button_add_appointment));
+	/* Switch tabs */
+	notebookMain.signal_switch_page().connect(
+		sigc::mem_fun(*this, &NotebookMain::on_notebook_switch_page));
+	/* Get Date */
+	buttonAddAppointment.signal_clicked().connect(
+		sigc::mem_fun(*this, &NotebookMain::on_button_add_appointment));
 
-    /* Add Main Window (Notebook) */
-    this->add(notebookMain);
-    this->show_all_children();
+	/* Add Main Window (Notebook) */
+	this->add(notebookMain);
+	this->show_all_children();
 }
 
 NotebookMain::~NotebookMain(){}
 
 void NotebookMain::on_notebook_switch_page(Gtk::Widget * /* page */, guint page_num){
-    std::cout << "Switched to tab with index : " << page_num << std::endl;
+	std::cout << "Switched to tab with index : " << page_num << std::endl;
 }
 
 void NotebookMain::on_button_add_appointment(){
-    std::cout << "Button clicked\nCreating new window\n...\n";
-    AddPet* window = new AddPet();
-    window->show();
-    // std::cout << calendar.get_date() << "\n"; This doesn't work!
+	std::cout << "Button clicked\nCreating new window\n...\n";
+	AddPet* window = new AddPet();
+	window->show();
+	// std::cout << calendar.get_date() << "\n"; This doesn't work!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,60 +19,60 @@ void printOptions();
 void printAbout();
 
 int main(int argc, char* argv[]){
-    /* GUI */
-    if(argc > 1){
-        /*
-         * I needed to create an aditional string to compare to.
-         * It tourns out that a char* and a string can't be compared.
-         */
-        std::string arg1(argv[1]);
-        if(arg1 == "-gui"){
-            auto app = Gtk::Application::create("dev.tyra");
-            NotebookMain notebookMain;
-            return app->run(notebookMain);
-        }
-    }
+	/* GUI */
+	if(argc > 1){
+		/*
+		* I needed to create an aditional string to compare to.
+		* It tourns out that a char* and a string can't be compared.
+		*/
+		std::string arg1(argv[1]);
+		if(arg1 == "-gui"){
+			auto app = Gtk::Application::create("dev.tyra");
+			NotebookMain notebookMain;
+			return app->run(notebookMain);
+		}
+	}
 
-    /* MENU */
-    char option;
-    bool quit = false;
-    printWelcomeMessage();
-    while(!quit){
-        printOptions();
-        std::cin >> option;
-        switch(option){
-            case ::a:
-                break;
-            case ::c:
-                break;
-            case ::q:
-                std::cout << "Exiting program...\n";
-                quit = true;
-                break;
-            case ::t:
-                printAbout();
-                break;
-            default:
-                std::cout << "Enter a valid option!\n\n";
-                break;
-        }
-    }
+	/* MENU */
+	char option;
+	bool quit = false;
+	printWelcomeMessage();
+	while(!quit){
+		printOptions();
+		std::cin >> option;
+		switch(option){
+			case ::a:
+				break;
+			case ::c:
+				break;
+			case ::q:
+				std::cout << "Exiting program...\n";
+				quit = true;
+				break;
+			case ::t:
+				printAbout();
+				break;
+			default:
+				std::cout << "Enter a valid option!\n\n";
+				break;
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
 /* Function implementation */
 void printWelcomeMessage(){ std::cout << "\tWelcome to Tyra!\n"; }
 
 void printOptions(){
-    std::cout << "\ta - Add new Pet\n";
-    std::cout << "\tc - Check DataBase\n";
-    std::cout << "\tq - Exit the program\n";
-    std::cout << "\tt - About\n\n";
+	std::cout << "\ta - Add new Pet\n";
+	std::cout << "\tc - Check DataBase\n";
+	std::cout << "\tq - Exit the program\n";
+	std::cout << "\tt - About\n\n";
 }
 
 void printAbout(){
-    std::cout << "\tTyra. Veterinary Management.\n";
-    std::cout << "\tBy Andres Ruiz\n";
-    std::cout << "\t" << ::WEB_PAGE << "\n";
+	std::cout << "\tTyra. Veterinary Management.\n";
+	std::cout << "\tBy Andres Ruiz\n";
+	std::cout << "\t" << ::WEB_PAGE << "\n";
 }
