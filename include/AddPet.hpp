ae61cd1d85bf391e9276fbc10306e844596679c9
Andres Ruiz <andrsruiz@gmail.com>
2020-05-04 12:25:59 -0500

    Change Spaces into Tabs

--- include/AddPet.hpp
+++ include/AddPet.hpp
@@ -3,21 +3,21 @@
 #include <gtkmm.h>
 
 class AddPet : public Gtk::Window{
-    public:
-        AddPet();
-        virtual ~AddPet();
+	public:
+		AddPet();
+		virtual ~AddPet();
 
-    protected:
-        // Signal handlers
-        void on_button_add_pet();
+	protected:
+		// Signal handlers
+		void on_button_add_pet();
 
-        // Member widgets
-        Gtk::Grid gridMain;
-        Gtk::Label labelPet, labelPetSpecie, labelPetBreed, labelPetName, labelPetBirthday,
-            labelOwnerName, labelOwnerLastName, labelOwnerGender, labelOwnerEmail,
-            labelOwnerAddress, labelOwnerPhoneNumber;
-        Gtk::Entry entryPetSpecie, entryPetBreed, entryPetName, entryPetBirthday,
-            entryOwnerName, entryOwnerLastName, entryOwnerGender, entryOwnerEmail,
-            entryOwnerAddress, entryOwnerPhoneNumber;
-        Gtk::Button buttonAddPet;
+		// Member widgets
+		Gtk::Grid gridMain;
+		Gtk::Label labelPet, labelPetSpecie, labelPetBreed, labelPetName, labelPetBirthday,
+			labelOwnerName, labelOwnerLastName, labelOwnerGender, labelOwnerEmail,
+			labelOwnerAddress, labelOwnerPhoneNumber;
+		Gtk::Entry entryPetSpecie, entryPetBreed, entryPetName, entryPetBirthday,
+			entryOwnerName, entryOwnerLastName, entryOwnerGender, entryOwnerEmail,
+			entryOwnerAddress, entryOwnerPhoneNumber;
+		Gtk::Button buttonAddPet;
 };
