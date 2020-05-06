e8d4551c8aec9b0bea5ec8866d61bda632faee41
Andres Ruiz <andrsruiz@gmail.com>
2020-05-06 17:07:53 -0500

    No Ncurses. Go Straight Qt

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,6 @@
 
 /* TYRA by Andres Ruiz */
 
-#include <iostream>
-#include <string>
-#include <ncurses.h>
-#include "../include/Tyra.hpp"
-#include "../include/Owner.hpp"
-#include "../include/Pet.hpp"
+int main(){
 
-const char a = 'a';
-const char c = 'c';
-const char q = 'q';
-const char t = 't';
-
-int main(int argc, char* argv[]){
-	initscr();
-
-	/* MENU */
-	char option = 't';
-	bool quit = false;
-	Tyra tyra;
-	refresh();
-	tyra.printWelcomeMessage();
-	while(!quit){
-		refresh();
-		tyra.printOptions();
-		std::cin >> option;
-		switch(option){
-			case ::a:
-				break;
-			case ::c:
-				break;
-			case ::q:
-				refresh();
-				std::cout << "Exiting program..." << std::endl;
-				quit = true;
-				break;
-			case ::t:
-				refresh();
-				tyra.printAbout();
-				break;
-			default:
-				refresh();
-				std::cout << "Enter a valid option!" << std::endl << std::endl;
-				break;
-		}
-	}
-
-	endwin();
-	return 0;
 }
