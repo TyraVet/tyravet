969ddbccb7bd6bdf1b2837cb773bfe64be4ef8fe
Andres Ruiz <andrsruiz@gmail.com>
2020-08-13 17:34:26 -0500

    Create NavBar Component

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='navbar'>
+	<b-button type='is-primary'
+			  icon-pack='fas'
+			  icon-left='bars'
+			  @click=openSideBar()>
+	  {{ menu }}
+	</b-button>
+	<h1 class='is-size-3'>{{ title }}</h1>
+	<SideBar v-if='user'></SideBar>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/NavBar.vue'
+
+export default {
+	name: 'NavBar',
+	components: { SideBar },
+	data() {
+		return {
+			title: 'Tyra Web',
+			menu: 'Menu',
+			user: null
+		}
+	},
+	methods: {
+		openSideBar() {
+			alert('Hi')
+		}
+	}
+}
+</script>
+
+<style>
+@import '../assets/css/navbar.css'
+</style>
