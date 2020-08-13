0212377ee5db1437e1c6d4f0d82fb8b5c9df40ee
Andres Ruiz <andrsruiz@gmail.com>
2020-08-13 17:35:09 -0500

    Add NavBar to App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
+	<NavBar></NavBar>
 	<router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from '@/components/NavBar.vue'
 
-#nav {
-  padding: 30px;
+export default {
+	name: 'app',
+	components: { NavBar }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
