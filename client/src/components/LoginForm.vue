4fecfa0afe587a3158183c7dccf41e1b7d7b0be3
Andres Ruiz <andrsruiz@gmail.com>
2020-08-13 11:30:32 -0500

    Add Login View

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='login-form'>
+	<form id='login-form'>
+	  <section>
+		<b-field :label='labelUsername'>
+		  <b-input v-model='username'></b-input>
+		</b-field>
+		<b-field :label='labelPassword'>
+		  <b-input type='password' v-model='password'></b-input>
+		</b-field>
+	  </section>
+	</form>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'LoginForm',
+	data() {
+		return {
+			labelUsername: 'Username',
+			labelPassword: 'Password',
+			username: '',
+			password: ''
+		}
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='login'>
+	<h1 class='has-text-primary'>{{ welcomeMessage }}</h1>
+	<LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+	name: 'Login',
+	components: { LoginForm },
+	data() {
+		return {
+			welcomeMessage: 'Tyra Web'
+		}
+	}
+}
+</script>
+
+<style></style>
