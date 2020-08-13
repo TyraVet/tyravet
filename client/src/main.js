a9fae15af819baea99302a6434be6c32913aa087
Andres Ruiz <andrsruiz@gmail.com>
2020-08-13 11:22:18 -0500

    Add no margin and no padding

--- client/src/main.js
+++ client/src/main.js
@@ -4,14 +4,15 @@ import router from './router'
 import Buefy from 'buefy'
 import './assets/scss/app.scss'
 import './variables.scss'
+import './assets/css/no-margin-nor-padding.css'
 
 Vue.use(Buefy, {
-  defaultIconPack: 'fas'
+	defaultIconPack: 'fas'
 })
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount('#app')
