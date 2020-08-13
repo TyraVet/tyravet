4177e8cb8a9a01637a8ad26f8e1afd9d1147cdd2
Andres Ruiz <andrsruiz@gmail.com>
2020-08-13 17:33:56 -0500

    Create SideBar Component

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+	<b-sidebar type="is-light"
+			   :fullheight="fullheight"
+			   :open.sync="open">
+	  <div class="p-1">
+		<b-menu>
+		  <b-menu-list label="Menu">
+			<b-menu-item icon="information-outline" label="Info"></b-menu-item>
+			<b-menu-item icon="settings">
+			  <b-menu-item icon="account" label="Users"></b-menu-item>
+			</b-menu-item>
+		  </b-menu-list>
+		</b-menu>
+	  </div>
+	</b-sidebar>
+  </section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			open: false,
+			overlay: true,
+			fullheight: true
+		}
+	}
+}
+</script>
+
+<style>
+.p-1 {
+  padding: 1em;
+}
+</style>
