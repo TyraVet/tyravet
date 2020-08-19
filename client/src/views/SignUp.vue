38f57fd4dcee9df180e945759ab975c70c5b2dea
Andres Ruiz <andrsruiz@gmail.com>
2020-08-19 12:26:33 -0500

    Add SignUp View

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='sign-up'>
+	<SignUpForm></SignUpForm>
+  </div>
+</template>
+
+<script>
+import SignUpForm from '@/components/SignUpForm.vue'
+
+export default {
+	name: 'SignUp',
+	components: { SignUpForm },
+	data() {
+		return {
+		}
+	}
+}
+</script>
