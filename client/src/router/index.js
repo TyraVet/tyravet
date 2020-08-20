652a69477198a8a2a3e6be9187bff2fe32149ae6
Andres Ruiz <andrsruiz@gmail.com>
2020-08-20 13:08:21 -0500

    Add Home and Not Found Views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 
@@ -7,11 +9,23 @@ Vue.use(VueRouter)
 
 const routes = [
 	{
+		path: '/',
+		name: 'home',
+		component: Home
+	},
+	{
+		path: '*',
+		name: 'page-not-found',
+		component: PageNotFound
+	},
+	{
 		path: '/log-in',
+		name: 'log-in',
 		component: Login
 	},
 	{
 		path: '/sign-up',
+		name: 'sign-up',
 		component: SignUp
 	}
 ]
