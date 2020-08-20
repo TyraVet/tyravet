d8a64affab589a77b3b47ca098813d1187037a2d
Andres Ruiz <andrsruiz@gmail.com>
2020-08-20 13:53:45 -0500

    Add PageNotFound View

---
+++ client/src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='page-not-found'>
+	<h1 class='is-size-1'>Page Not Found</h1>
+	<button @click=goBack()>Go Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'PageNotFound',
+	methods: {
+		goBack(){
+			if(this.$router)
+				this.$router.go(-1)
+		}
+	}
+}
+</script>
