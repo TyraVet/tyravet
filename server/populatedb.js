bd4051b107dcaac8e5d1c5e908846bb5f3dd0dc9
Andres Ruiz <andrsruiz@gmail.com>
2020-08-21 13:07:52 -0500

    Add populateDB script to add admin user

---
+++ server/populatedb.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+
+const async = require('async')
+const bcryptjs = require('bcryptjs')
+const User = require('./models/user')
+
+var mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_TYRAWEB_TEST, { useNewUrlParser: true })
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+var users = []
+
+function userCreate(username, password, callback){
+	userdetail = {
+		username : username,
+		password: password
+	}
+
+	var myUser = new User(userdetail)
+
+	bcryptjs.hash(myUser.password, 10, (err, hashedPassword) => {
+		if(err)
+			res.sendStatus(401)
+
+		/* Success */
+		const user = new User({
+			username: myUser.username,
+			password: hashedPassword
+		}).save(err => {
+			if(err){
+				callback(err, null)
+				return
+			}
+			console.log('New User: ' + myUser)
+			users.push(myUser)
+			callback(null, myUser)
+		})
+	})
+}
+
+function createUsers(callback) {
+	async.series([
+		function(callback) {
+			userCreate('admin', 'admin12', callback)
+		}
+	], callback)
+}
+
+async.series([
+	createUsers
+], function(err, results){
+	if (err) {
+		console.log('FINAL ERR: ' + err)
+	}
+	else {
+		console.log('Users: ' + users)
+	}
+	// All done, disconnect from database
+	mongoose.connection.close()
+})
