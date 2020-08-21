d6fb0890aa3f996ac90a0e0a8be2559d312d05a3
Andres Ruiz <andrsruiz@gmail.com>
2020-08-21 15:41:58 -0500

    Add Type Attribute to User Model

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema
 var UserSchema = new Schema({
 	username: { type: String, required: true },
 	password: { type: String, required: true },
+	type: { type: Object, required: true },
 	created: { type: Date, default: Date.now }
 })
 
