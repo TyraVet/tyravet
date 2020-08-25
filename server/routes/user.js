bc0dcdc77b86c2ffde93ffb44d04b4f5376fea53
Andres Ruiz <andrsruiz@gmail.com>
2020-08-25 12:46:37 -0500

    Typo

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,7 +6,7 @@ const authenticateToken = require('../middlewares/authenticateToken.js')
 /* Require Controller Modules */
 var userController = require(path.join(__dirname, '../controllers/user.js'))
 
-/* Character Routes */
+/* User Routes */
 router.post('/find', authenticateToken, userController.post_find_user_by_id)
 router.post('/login', userController.post_find_user)
 router.post('/signup', authenticateToken, userController.post_create_user)
