9f6b28e45f8b95d1ca3cb4db09a13b319e9dbb67
Andres Ruiz <andrsruiz@gmail.com>
2020-08-25 13:27:59 -0500

    Create Breed Routes and add POST to create one

---
+++ server/routes/breed.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const authenticateToken = require('../middlewares/authenticateToken.js')
+
+/* Require Controller Modules */
+var breedController = require(path.join(__dirname, '../controllers/breed.js'))
+
+/* Breed Routes */
+router.post('/create', authenticateToken, breedController.post_create_breed)
+
+module.exports = router
