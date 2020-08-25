dff6d758797f8075ee14a6029c1f85515dbad907
Andres Ruiz <andrsruiz@gmail.com>
2020-08-25 16:49:58 -0500

    Create Breed View

---
+++ client/src/views/Breeds.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='breeds'>
+	<span class='title-container'>
+	  <h3 class='is-size-4 has-text-primary-dark'>Breeds</h3>
+	  <b-button type='is-primary'
+				icon-pack='fas'
+				icon-left='plus'>
+		Add
+	  </b-button>
+	</span>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Breeds'
+}
+</script>
+
+<style>
+@import '../assets/css/titles.css'
+</style>
