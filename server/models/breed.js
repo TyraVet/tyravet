b82ff989d792d2869508920025f96a6256d6ff66
Andres Ruiz <andrsruiz@gmail.com>
2020-08-25 10:53:37 -0500

    Add Breed Schema

---
+++ server/models/breed.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var BreedSchema = new Schema({
+	name: { type: String, required: true }
+})
+
+module.exports = mongoose.model('Breed', BreedSchema)
