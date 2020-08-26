2731cfcd3f0b54cc09291aa51301e7b937c0485f
Andres Ruiz <andrsruiz@gmail.com>
2020-08-26 17:25:39 -0500

    Get Breeds in Client

---
+++ client/src/components/BreedList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+	<h1>List</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	name: 'BreedList',
+	data() {
+		return {
+			breeds: []
+		}
+	},
+	methods: {
+		init(){
+			console.log(this.$store.state.user.token)
+			this.getBreeds()
+		},
+		getBreeds(){
+			axios.get(process.env.VUE_APP_TYRAWEB_BREEDS, {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.user.token
+				}
+			}).then((response) => {
+				console.log(response)
+			}).catch((error) => {
+				console.error(error)
+			})
+		}
+	},
+	mounted(){
+		this.init()
+	}
+}
+</script>
