8400121dde064bb0e4da98f264e28b530cf609f8
Andres Ruiz <andrsruiz@gmail.com>
2020-08-26 11:43:45 -0500

    Remove Unnecessary Comparation

--- server/controllers/breed.js
+++ server/controllers/breed.js
@@ -2,10 +2,6 @@ const Breed = require('../models/breed.js')
 
 /* Create Breed */
 exports.post_create_breed = (req, res, next) => {
-	if(err)
-		res.sendStatus(401)
-
-	/* Success */
 	const breed = new Breed({
 		name: req.body.name,
 	}).save(err => {
