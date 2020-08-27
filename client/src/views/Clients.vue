385a2ef29bf23a407d45c45bba262123259293f4
Andres Ruiz <andrsruiz@gmail.com>
2020-08-27 18:00:07 -0500

    Add Clients View

---
+++ client/src/views/Clients.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+	<ClientList></ClientList>
+  </div>
+</template>
+
+<script>
+import ClientList from '@/components/ClientList.vue'
+
+export default {
+	name: 'Clients',
+	components: { ClientList }
+}
+</script>
