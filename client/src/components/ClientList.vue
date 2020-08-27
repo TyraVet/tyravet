c55480b51b85bb11030d75ce6647e43c46d1438e
Andres Ruiz <andrsruiz@gmail.com>
2020-08-27 18:00:18 -0500

    Add ClientsList Component

---
+++ client/src/components/ClientList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='client-list'>
+	<span class='no-clients'
+		  v-if='clients.length == 0'>
+	  <b-message type='is-danger'
+				 icon-pack='fas'
+				 icon-size='is-large'
+				 icon='exclamation-circle'
+				 has-icon
+				 class='message'>
+	  <h1 class='has-text-danger'>{{ noClients }}</h1>
+	</b-message>
+
+	</span>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'ClientList',
+	data(){
+		return{
+			clients: [],
+			noClients: "You don't have Clients now. Add One!"
+		}
+	}
+}
+</script>
+
+<style>
+.no-clients {
+  margin: 50px;
+ }
+</style>
