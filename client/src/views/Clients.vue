8f8235ff1987979484e55277837c6792f46b43c7
Andres Ruiz <andrsruiz@gmail.com>
2020-08-28 13:19:41 -0500

    Launch Modal on Button Click

--- client/src/views/Clients.vue
+++ client/src/views/Clients.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
+  <div class='clients'>
+	<span class='title-container'>
+	  <h3 class='is-size-4 has-text-primary-dark'>{{ title }}</h3>
+	  <b-button type='is-primary'
+				icon-pack='fas'
+				icon-left='plus'
+				@click=launchModal()>
+		{{ labelButton }}
+	  </b-button>
+	</span>
 	<ClientList></ClientList>
   </div>
 </template>
 
 <script>
 import ClientList from '@/components/ClientList.vue'
+import ClientForm from '@/components/ClientForm.vue'
 
 export default {
 	name: 'Clients',
-	components: { ClientList }
+	components: { ClientList },
+	data(){
+		return{
+			title: 'Clients',
+			labelButton: 'Add'
+		}
+	},
+	methods: {
+		launchModal(){
+			this.$buefy.modal.open({
+				parent: this,
+				component: ClientForm,
+				hasModalCard: true,
+				trapFocus: true
+			})
+		}
+	}
 }
 </script>
