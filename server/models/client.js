485357a9dd94328f32dcdb347ca53edef6550f0d
Andres Ruiz <andrsruiz@gmail.com>
2020-08-28 15:09:52 -0500

    Change type of Attributes

--- server/models/client.js
+++ server/models/client.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
+const AddressSchema = require('./address.js')
+const PetSchema = require('./pet.js')
 
 var Schema = mongoose.Schema
 
 var ClientSchema = new Schema({
 	name: { type: String, required: true },
-	pets: { type: Array, required: true, default: [] },
-	address: { type: Object, default: {} },
+	pets: { type: [PetSchema], required: true },
+	address: { type: AddressSchema, default: {} },
 	phone: { type: Number, required: true }
 })
 
