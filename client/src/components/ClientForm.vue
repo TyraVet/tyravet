a9ee6f71042f436fd7c4dfba81227b9cb7c5c4e2
Andres Ruiz <andrsruiz@gmail.com>
2020-08-28 13:19:55 -0500

    Add Client Form

---
+++ client/src/components/ClientForm.vue
@@ -0,0 +1,110 @@
+<template>
+  <form id='client-form'>
+	<div class='modal-card' style='width: auto'>
+	  <header class='modal-card-head'>
+		<p class='modal-card-title'>{{ title }}</p>
+		<button type='button'
+				class='delete'
+				@click=close() />
+	  </header>
+	  <section class='modal-card-body'>
+		   <b-field label='Name'>
+			 <b-input type='text'
+					  v-model='clientName'
+					  required>
+			 </b-input>
+		   </b-field>
+		   <b-field label='Address'>
+		   </b-field>
+	  </section>
+	  <footer class='modal-card-foot'>
+		<button class='button'
+				type='button'
+				@click=close()>
+		  Cancel
+		</button>
+		<b-button class='button is-success'
+				@click=send()>
+		  Accept
+		</b-button>
+		<b-message title='Success'
+				   type='is-success'
+				   aria-close-label='Close message'
+				   icon-pack='fas'
+				   icon-size='is-medium'
+				   icon='check'
+				   has-icon
+				   auto-close
+				   class='message'
+				   v-if='status === 201 || status === 200'>
+		  {{ statusText }}
+		</b-message>
+		<b-message title='Error'
+				   type='is-danger'
+				   aria-close-label='Close message'
+				   icon-pack='fas'
+				   icon-size='is-medium'
+				   icon='exclamation'
+				   has-icon
+				   auto-close
+				   class='message'
+				   v-if='status === 401 || status === 404'>
+		  {{ error }}
+		</b-message>
+	  </footer>
+	</div>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	name: 'ClientForm',
+	data() {
+		return {
+			title: 'Create Client',
+			clientName: '',
+			status: null,
+			statusText: '',
+			error: ''
+		}
+	},
+	methods: {
+		clearInput(){
+			this.clientName = ''
+		},
+		setOnSuccess(response){
+			this.status = response.status
+			this.statusText = response.statusText
+			this.clearInput()
+		},
+		setOnError(error){
+			if(error.response){
+				this.status = error.response.status
+				this.error = error.response.statusText
+			}else if(error.request){
+				this.error = error.request
+			}else{
+				this.error = error.message
+			}
+		},
+		close(){
+			this.$emit('close')
+		},
+		send(){
+			axios.post(process.env.VUE_APP_TYRAWEB_CREATE_CLIENT, {
+				name: this.clientName
+			}, {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.user.token
+				}
+			}).then((response) => {
+				this.setOnSuccess(response)
+			}).catch((error) => {
+				this.setOnError(error)
+			})
+		}
+	}
+}
+</script>
