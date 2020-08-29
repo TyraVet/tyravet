1259e263cc019ca6546b4771a1c064133b9e8d67
Andres Ruiz <andrsruiz@gmail.com>
2020-08-29 11:39:27 -0500

    Set POST method to create Client

---
+++ server/controllers/client.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const Client = require('../models/client.js')
+const Pet = require('../models/pet.js')
+const Address = requie('../models/address.js')
+
+/* Create Client */
+exports.post_create_client = (req, res, next) => {
+	const address = new Address({
+		street: req.body.street,
+		number: req.body.number,
+		intNumber: req.body.intNumber,
+		postalCode: req.body.postalCode
+	})
+
+	const pet = new Pet({
+		name: req.body.petName,
+		birthday: req.body.petBirthday,
+		age: req.body.petAge,
+		weight: req.body.petWeight,
+		breed: req.body.petBreed
+	})
+
+	const pets = []
+	pets.push(pet)
+
+	const client = new Client({
+		name: req.body.name,
+		phone: req.body.phone,
+		pets: pets,
+		address: address
+	}).save(err => {
+		if(err)
+			return next(err)
+
+		/* Success */
+		res.sendStatus(201).json(client)
+	})
+}
