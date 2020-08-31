7dd206749f2a5aa8143548de30e2a469a4b8b0bf
Andres Ruiz <andrsruiz@gmail.com>
2020-08-31 17:17:25 -0500

    Display Todays date

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class='home'>
-	<h1 class='is-size-1'>Home</h1>
+	<h1 class='is-size-5'>Today is {{ today }}</h1>
+	<AppointmentsList></AppointmentsList>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import AppointmentsList from '@/components/AppointmentsList.vue'
+
 export default {
-	name: 'Home'
+	name: 'Home',
+	components: { AppointmentsList },
+	data(){
+		return{
+			today: moment(new Date()).format('MMMM Do YYYY')
+		}
+	}
 }
 </script>
