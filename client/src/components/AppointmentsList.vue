2fdee555bf09ce2caf0648a7f5f9bbc29a2af56a
Andres Ruiz <andrsruiz@gmail.com>
2020-09-01 10:35:23 -0500

    Fill Hours Array

--- client/src/components/AppointmentsList.vue
+++ client/src/components/AppointmentsList.vue
@@ -1,12 +1,32 @@
 <template>
   <div class='appointments-list'>
-	<div>
-	</div>
   </div>
 </template>
 
 <script>
 export default {
-	name: 'AppointmentsList'
+	name: 'AppointmentsList',
+	data(){
+		return{
+			hours: []
+		}
+	},
+	methods: {
+		/* Fill the hours array with the available hours
+		 * where you can set an appointment. */
+		init(){
+			const maxLength = 13
+			const initialHour = 8
+
+			for(let i = 0; i < maxLength; i++){
+				if(i !== 0)
+					this.hours.push((initialHour + i) + ':00')
+				this.hours.push((initialHour + i) + ':30')
+			}
+		}
+	},
+	mounted(){
+		this.init()
+	}
 }
 </script>
