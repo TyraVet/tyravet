d6e3fb65e9e058974da16b13779a6c9b7aa299f5
Andres Ruiz <andrsruiz@gmail.com>
2020-09-02 12:09:18 -0500

    Add Price Attribute

--- server/models/service.js
+++ server/models/service.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var ServiceSchema = new Schema({
-	name: { type: String, required: true }
+	name: { type: String, required: true },
+	price: { type: Number, required: true }
 })
 
 module.exports = mongoose.model('Service', ServiceSchema)
