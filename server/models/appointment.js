e29c621fb6b5dfed99928f5e868f29169507cbd2
Andres Ruiz <andrsruiz@gmail.com>
2020-09-02 17:03:09 -0500

    Cerate Appointment Model and Schema

---
+++ server/models/appointment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Service = require('./schemas/service.js')
+const Client = require('./schemas/client.js')
+
+var Schema = new mongoose.Schema
+
+const AppointmentSchema = new Schema({
+	service: { type: Service, required: true },
+	client: { type: Client, required: true },
+	hour: { type: String, required: true }
+})
+
+module.exports = mongoose.model('Appointment', AppointmentSchema)
