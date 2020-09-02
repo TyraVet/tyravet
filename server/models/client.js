28fa02cb26adb023c7f8bc19b63b94414eda37a6
Andres Ruiz <andrsruiz@gmail.com>
2020-09-02 13:46:50 -0500

    Rename Attributes's types

--- server/models/client.js
+++ server/models/client.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-const AddressSchema = require('./schemas/address.js')
-const PetSchema = require('./schemas/pet.js')
+const Address = require('./schemas/address.js')
+const Pet = require('./schemas/pet.js')
 
 var Schema = mongoose.Schema
 
 var ClientSchema = new Schema({
 	name: { type: String, required: true },
 	phone: { type: Number, required: true },
-	pets: { type: [PetSchema], required: true },
-	address: { type: AddressSchema, default: {} }
+	pets: { type: [Pet], required: true },
+	address: { type: Address, default: {} }
 })
 
 module.exports = mongoose.model('Client', ClientSchema)
