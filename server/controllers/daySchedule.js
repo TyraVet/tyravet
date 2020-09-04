571057d738ebb080da0c8dc86a6525bcae2ce24a
Andres Ruiz <andrsruiz@gmail.com>
2020-09-04 16:35:51 -0500

    Body is undefined, why?

--- server/controllers/daySchedule.js
+++ server/controllers/daySchedule.js
@@ -2,28 +2,29 @@ const DaySchedule = require('../models/daySchedule.js')
 
 /* GET Find the current DaySchedule */
 exports.get_day_schedule = (req, res, next) => {
-	DaySchedule.findOne({ date: req.body.date }, (err, daySchedule) => {
+	console.log(req.body) // is undefined, why?
+	DaySchedule.findOne({ date: new Date('2020-09-04') }, (err, daySchedule) => {
 		if(err)
-			res.sendStatus(403).end()
+			res.status(403).json(err)
 
 		if(!daySchedule)
-			res.sendStatus(404)
+			res.status(404).end()
 		else
 			/* Success */
-			res.status(201).json(daySchedule)
+			res.status(200).json(daySchedule)
 	})
 }
 
 /* POST Create DaySchedule */
 exports.post_create_day_schedule = (req, res, next) => {
 	const daySchedule = new DaySchedule({
-		date: body.date,
+		date: req.body.date,
 		appointments: []
 	}).save(err => {
 		if(err)
-			res.sendStatus(403)
+			res.status(403).json(err)
 
 		/* Success */
-		res.sendStatus(201).json(daySchedule)
+		res.status(201).json(daySchedule)
 	})
 }
