fcc1e7b93d6d7753ef8f234b902439e81ad69ea7
Andres Ruiz <andrsruiz@gmail.com>
2020-09-05 14:02:40 -0500

    Add to Router to add appointment

--- server/routes/daySchedule.js
+++ server/routes/daySchedule.js
@@ -9,5 +9,6 @@ var daySchedule = require(path.join(__dirname, '../controllers/daySchedule.js'))
 /* DaySchedule Routes */
 router.get('/', authenticateToken, daySchedule.get_day_schedule)
 router.post('/create-day-schedule', authenticateToken, daySchedule.post_create_day_schedule)
+router.post('/add-appointment', authenticateToken, daySchedule.post_add_appointment)
 
 module.exports = router
