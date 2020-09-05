cc2d901eede9b289082425943bf39c9127c819b2
Andres Ruiz <andrsruiz@gmail.com>
2020-09-05 14:02:30 -0500

    Add POST method to add appointment

--- server/controllers/daySchedule.js
+++ server/controllers/daySchedule.js
@@ -1,4 +1,5 @@
 const DaySchedule = require('../models/daySchedule.js')
+const Appointment = require('../models/appointment.js')
 
 /* GET Find the current DaySchedule */
 exports.get_day_schedule = (req, res, next) => {
@@ -20,6 +21,31 @@ exports.post_create_day_schedule = (req, res, next) => {
 		date: req.body.date,
 		appointments: []
 	}).save((err, theDaySchedule) => {
+		if(err)
+			res.status(403).json(err)
+
+		/* Success */
+		res.status(201).json(theDaySchedule)
+	})
+}
+
+/* POST Add Appointment into the Schedule */
+exports.post_add_appointment = (req, res, next) => {
+	if(!req.body.service || !req.body.client)
+		res.status(406).json({ error: 'No Service or Client send' })
+
+	const appointment = new Appointment({
+		service: req.body.service,
+		client: req.body.client,
+		hour: req.body.hour
+	})
+
+	let appointments = req.body.appointments
+	appointments.push(appointment)
+
+	DaySchedule.findByIdAndUpdate(req.body.id,
+								  { appointments: appointments },
+								  (err, theDaySchedule) => {
 		if(err)
 			res.status(403).json(err)
 
