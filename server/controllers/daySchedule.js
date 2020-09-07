d1fa1dfa89e986fb2efa20156d74c5100b046fc2
Andres Ruiz <andrsruiz@gmail.com>
2020-09-07 13:54:27 -0500

    Merged two methods together
    If there is no schedule of 'today', create a new one

--- server/controllers/daySchedule.js
+++ server/controllers/daySchedule.js
@@ -1,31 +1,27 @@
 const DaySchedule = require('../models/daySchedule.js')
 const Appointment = require('../models/appointment.js')
 
-/* GET Find the current DaySchedule */
-exports.get_day_schedule = (req, res, next) => {
-	DaySchedule.findOne({ date: new Date(req.query.date) }, (err, daySchedule) => {
+/* POST Find the current DaySchedule, if there are none, create new */
+exports.post_day_schedule = (req, res, next) => {
+	DaySchedule.findOne({ date: new Date(req.body.date) }, (err, daySchedule) => {
 		if(err)
 			res.status(403).json(err)
 
-		if(!daySchedule)
-			res.status(404).end()
-		else
+		if(!daySchedule){
+			const newDaySchedule = new DaySchedule({
+				date: req.body.date,
+				appointments: []
+			}).save((err, theDaySchedule) => {
+				if(err)
+					res.status(403).json(err)
+
+				/* Success */
+				res.status(201).json(theDaySchedule)
+			})
+		}else{
 			/* Success */
 			res.status(200).json(daySchedule)
-	})
-}
-
-/* POST Create DaySchedule */
-exports.post_create_day_schedule = (req, res, next) => {
-	const daySchedule = new DaySchedule({
-		date: req.body.date,
-		appointments: []
-	}).save((err, theDaySchedule) => {
-		if(err)
-			res.status(403).json(err)
-
-		/* Success */
-		res.status(201).json(theDaySchedule)
+		}
 	})
 }
 
@@ -45,11 +41,11 @@ exports.post_add_appointment = (req, res, next) => {
 
 	DaySchedule.findByIdAndUpdate(req.body.id,
 								  { appointments: appointments },
-								  (err, theDaySchedule) => {
+								  err => {
 		if(err)
 			res.status(403).json(err)
 
 		/* Success */
-		res.status(201).json(theDaySchedule.appointments)
+		res.status(201).json(appointments)
 	})
 }
