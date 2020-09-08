6ad795901817811fdb5787f5d63f632664a60ea4
Andres Ruiz <andrsruiz@gmail.com>
2020-09-08 17:31:38 -0500

    User Appointments Header Comp in Home View

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,26 +1,19 @@
 <template>
   <div class='home'>
-	<h1 class='is-size-4 has-text-centered has-text-weight-semibold'>{{ formattedToday }}</h1>
+	<AppointmentsHeader></AppointmentsHeader>
 	<AppointmentsList></AppointmentsList>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import AppointmentsHeader from '@/components/AppointmentsHeader.vue'
 import AppointmentsList from '@/components/AppointmentsList.vue'
 
 export default {
 	name: 'Home',
-	components: { AppointmentsList },
-	data(){
-		return{
-			formattedToday: moment(this.today).format('MMMM Do YYYY')
-		}
-	},
-	computed: {
-		today(){
-			return this.$store.state.today
-		}
+	components: {
+		AppointmentsHeader,
+		AppointmentsList
 	}
 }
 </script>
