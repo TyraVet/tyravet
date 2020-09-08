bd209ead0362d73ef263e7513c35a54118e10129
Andres Ruiz <andrsruiz@gmail.com>
2020-09-08 17:30:25 -0500

    Add Appointments List Test

---
+++ client/tests/unit/appointments-list.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import moment from 'moment'
+import Buefy from 'buefy'
+import store from '@/store'
+import AppointmentsList from '@/components/AppointmentsList.vue'
+
+/* We import createLocalVue to load Buefy and store */
+const localVue = createLocalVue()
+localVue.use(Buefy)
+
+const wrapper = shallowMount(AppointmentsList, { store, localVue })
+
+describe('AppointmentsList Component', () => {
+	it('Set the correct default data', () => {
+		expect(typeof AppointmentsList.data).toBe('function')
+		const defaultData = AppointmentsList.data()
+		expect(defaultData.schedule).toMatchObject({})
+		expect(defaultData.hours).toEqual(expect.arrayContaining([]))
+	})
+})
