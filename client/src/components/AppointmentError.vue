2dd3706a9e38f2a5b638fcfd6c83f1521a22bd7c
Andres Ruiz <andrsruiz@gmail.com>
2020-09-10 10:27:32 -0500

    Create AppointmentError Component

---
+++ client/src/components/AppointmentError.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-message type='is-danger'
+			 icon-pack='fas'
+			 icon-size='is-medium'
+			 icon='exclamation'
+			 has-icon>
+	{{ message }}
+  </b-message>
+</template>
+
+<script>
+export default {
+	name: 'AppointmentError',
+	data(){
+		return{
+			message: 'Error: You can not create new appointments on passed days'
+		}
+	}
+}
+</script>
