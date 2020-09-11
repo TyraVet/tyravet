c972f2c1b2c6101b86a9c55df19ec7f35e2d2195
Andres Ruiz <andrsruiz@gmail.com>
2020-09-11 11:58:41 -0500

    Create Appointment Component

---
+++ client/src/components/Appointment.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='appointment'>
+	<section>
+	  <div class='field'>
+		<b-checkbox type='is-success'
+					v-model='isDone'>
+		  <div class='appointment-container' :class='{ done: isDone }'>
+			<h3 class='is-size-5 has-text-primary'>
+			  {{ appointment.service.name }}
+			</h3>
+			<h3 class='is-size-5 has-text-dark'>
+			  |
+			  {{ appointment.client.pets[0].name }},
+			  ({{ appointment.client.pets[0].breed.name }})
+			  -
+			</h3>
+			<h3 class='is-size-5 has-text-grey-darker'>
+			  {{ appointment.client.name }}, {{ appointment.client.phone }}
+			</h3>
+		  </div>
+		</b-checkbox>
+	  </div>
+	</section>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Appointment',
+	props: {
+		appointment: Object
+	},
+	data(){
+		return{
+			isDone: false
+		}
+	}
+}
+</script>
+
+<style>
+@import '../assets/css/appointment.css'
+</style>
