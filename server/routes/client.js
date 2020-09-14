626f583ce8702b026f8fcee7b8b84458c383a695
Andres Ruiz <andrsruiz@gmail.com>
2020-09-14 11:47:03 -0500

    Set Route for add pet POST method

--- server/routes/client.js
+++ server/routes/client.js
@@ -9,5 +9,6 @@ var clientController = require(path.join(__dirname, '../controllers/client.js'))
 /* Client Routes */
 router.post('/create', authenticateToken, clientController.post_create_client)
 router.get('/', authenticateToken, clientController.get_clients)
+router.post('/add-pet', authenticateToken, clientController.post_add_pet)
 
 module.exports = router
