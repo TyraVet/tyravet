449eefb8d40663a6f8e7cdfbb84c5c35a7a57754
Andres Ruiz <andrsruiz@gmail.com>
2020-09-14 11:46:45 -0500

    Add POST method to add a pet to a client

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -49,3 +49,33 @@ exports.get_clients = (req, res, next) => {
 			  res.json(clients)
 		  })
 }
+
+/* Add Pet to Client */
+exports.post_add_pet = (req, res, next) => {
+	const pet = new Pet({
+		name: req.body.petName,
+		birthday: req.body.petBirthday,
+		age: req.body.petAge,
+		weight: req.body.petWeight,
+		breed: req.body.petBreed
+	})
+
+	Client.findById(req.body.id, (err, client) => {
+		if(err)
+			res.status(403).json(err)
+
+		/* Success */
+		let pets = client.pets
+		pets.push(pet)
+
+		Client.findByIdAndUpdate(req.body.id,
+								{ pets: pets },
+								(err, theClient) => {
+			if(err)
+				res.status(403).json(err)
+
+			/* Success */
+			res.status(201).json(theClient)
+		})
+	})
+}
