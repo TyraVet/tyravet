2b67cdcfa824c066ec6d4ce95b78171301bb5e84
Andres Ruiz <andrsruiz@gmail.com>
2020-09-15 12:34:28 -0500

    Just store the client and pet id in appointment

--- server/models/schemas/appointment.js
+++ server/models/schemas/appointment.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const Service = require('./service.js')
-const Client = require('./client.js')
 
 var Schema = mongoose.Schema
 
 const AppointmentSchema = new Schema({
 	service: { type: Service, required: true },
-	client: { type: Client, required: true },
+	clientId: { type: String, required: true },
+	petId: { type: String, required: true },
 	hour: { type: String, required: true },
 	done: { type: Boolean, default: false }
 })
