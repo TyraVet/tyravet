6b7c056e7116afd252c8a7eaf51861ea9ce1f7b3
Andres Ruiz <andrsruiz@gmail.com>
2020-09-15 14:31:08 -0500

    Update PageNotFound Test to load buefy

--- client/tests/unit/page-not-found.spec.js
+++ client/tests/unit/page-not-found.spec.js
@@ -1,10 +1,12 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import router from '@/router'
+import Buefy from 'buefy'
 import PageNotFound from '@/views/PageNotFound.vue'
 
 /* We import createLocalVue to load VueRouter */
 
 const localVue = createLocalVue()
+localVue.use(Buefy)
 const wrapper = shallowMount(PageNotFound, { router, localVue })
 
 describe('PageNotFound Component', () => {
