6d172c9b835377d42f4cd2a33ea91b1c58cde551
Andres Ruiz <andrsruiz@gmail.com>
2020-09-15 12:36:00 -0500

    Add route to find client

--- server/routes/client.js
+++ server/routes/client.js
@@ -9,6 +9,7 @@ var clientController = require(path.join(__dirname, '../controllers/client.js'))
 /* Client Routes */
 router.post('/create', authenticateToken, clientController.post_create_client)
 router.get('/', authenticateToken, clientController.get_clients)
+router.get('/client', authenticateToken, clientController.get_client)
 router.post('/add-pet', authenticateToken, clientController.post_add_pet)
 
 module.exports = router
