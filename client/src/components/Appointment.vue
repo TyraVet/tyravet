576fc2de431e26a2104bb4acf44a269de13d400c
Andres Ruiz <andrsruiz@gmail.com>
2020-09-15 12:37:50 -0500

    Change way of get appointments from API

--- client/src/components/Appointment.vue
+++ client/src/components/Appointment.vue
@@ -9,13 +9,10 @@
 			  {{ appointment.service.name }}
 			</h3>
 			<h3 class='is-size-5 has-text-dark'>
-			  |
-			  {{ appointment.client.pets[0].name }},
-			  ({{ appointment.client.pets[0].breed.name }})
-			  -
+			  | {{ pet.name }}, ({{ pet.breed.name }}) -
 			</h3>
 			<h3 class='is-size-5 has-text-grey-darker'>
-			  {{ appointment.client.name }}, {{ appointment.client.phone }}
+			  {{ client.name }}, {{ client.phone }}
 			</h3>
 		  </div>
 		</b-checkbox>
@@ -25,6 +22,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { EventBus } from '../eventBus.js'
 
 export default {
@@ -34,22 +32,54 @@ export default {
 	},
 	data(){
 		return{
-			isDone: this.appointment.done
+			isDone: this.appointment.done,
+			client: null,
+			pet: null
 		}
 	},
-	methods: {
-		/* Emit event to update the specific appointment */
-		send(){
-			EventBus.$emit('update-appointments', {
-				appointment: this.appointment,
-				done: this.isDone
-			})
+	computed: {
+		user(){
+			return this.$store.state.user
 		}
 	},
 	watch: {
 		isDone(){
 			this.send()
 		}
+	},
+	methods: {
+		init(){
+			this.getClient()
+		},
+		/* Emit event to update the specific appointment */
+		send(){
+			EventBus.$emit('update-appointments', {
+				appointment: this.appointment,
+				done: this.isDone
+			})
+		},
+		getClient(){
+			axios.get(process.env.VUE_APP_TYRAWEB_CLIENT, {
+				params: {
+					id: this.appointment.clientId
+				},
+				headers: {
+					Authorization: 'Bearer ' + this.user.token
+				}
+			}).then(response => {
+				this.client = response.data
+
+				this.client.pets.forEach(pet => {
+					if(pet._id === this.appointment.petId)
+						this.pet = pet
+				})
+			}).catch(error => {
+				console.error(error)
+			})
+		}
+	},
+	created(){
+		this.init()
 	}
 }
 </script>
