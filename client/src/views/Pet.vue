74e4da6aa50752e74cefcf3bc4a3580ad77e9ae2
Andres Ruiz <andrsruiz@gmail.com>
2020-09-19 14:32:19 -0500

    Add Pet view w/Tests

---
+++ client/src/views/Pet.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='pet'>
+	<PetProfile :id='$route.params.id'></PetProfile>
+  </div>
+</template>
+
+<script>
+import PetProfile from '@/components/PetProfile.vue'
+
+export default {
+	name: 'Pet',
+	components: { PetProfile }
+}
+</script>
