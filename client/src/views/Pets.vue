a65c78be7a1979a1bcc0ffd845442dbcce8b9f5a
Andres Ruiz <andrsruiz@gmail.com>
2020-09-21 17:47:39 -0500

    Add Pets View

---
+++ client/src/views/Pets.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='pets'>
+	<PetsList></PetsList>
+  </div>
+</template>
+
+<script>
+import PetsList from '@/components/PetsList.vue'
+
+export default {
+	name: 'Pets',
+	components: { PetsList }
+}
+</script>
