b53b65a84793a0fce41bf525557b7af9075cc42e
Andres Ruiz <andrsruiz@gmail.com>
2020-09-22 16:17:50 -0500

    Update PetProfile Tests

--- client/tests/unit/pet-profile.spec.js
+++ client/tests/unit/pet-profile.spec.js
@@ -1,8 +1,20 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import store from '@/store'
 import PetProfile from '@/components/PetProfile.vue'
 
 const localVue = createLocalVue()
-const wrapper = shallowMount(PetProfile, { propsData: { id: '' }, localVue })
+
+/* dummy user */
+store.state.user = {
+	_id: '',
+	username: '',
+	type: {},
+	token: ''
+}
+
+const wrapper = shallowMount(PetProfile, { store,
+										   propsData: { id: '' },
+										   localVue })
 
 describe('Pet Component', () => {
 	it('Renders a main div container', () => {
@@ -11,5 +23,25 @@ describe('Pet Component', () => {
 
 	it('Should have an ID prop data', () => {
 		expect(wrapper.props('id')).toMatch('')
+	})
+
+	it('Should set the default data', async () => {
+		expect(typeof PetProfile.data).toBe('function')
+		const data = PetProfile.data()
+		expect(data.pet).toMatchObject({
+			name: '',
+			breed: {
+				name: ''
+			}
+		})
+		expect(data.owner).toMatchObject({
+			name: '',
+			phone: '',
+			address: {
+				street: '',
+				number: '',
+				postalCode: ''
+			}
+		})
 	})
 })
