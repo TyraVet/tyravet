c91f67062011dc5c5fe5e0db8535e636f5b538d5
Andres Ruiz <andrsruiz@gmail.com>
2020-09-23 14:03:49 -0500

    Init Vaccination Record Comp

---
+++ client/src/components/VaccinationRecord.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='vaccination-record'>
+	<VaccinationTitle></VaccinationTitle>
+	<VaccinationList></VaccinationList>
+  </div>
+</template>
+
+<script>
+import VaccinationTitle from './VaccinationTitle.vue'
+import VaccinationList from './VaccinationList.vue'
+
+export default {
+	name: 'VaccinationRecord',
+	components: { VaccinationTitle, VaccinationList }
+}
+</script>
