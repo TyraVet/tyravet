ffe22e8de75956166d15596de3cdb70971a5b448
Andres Ruiz <andrsruiz@gmail.com>
2020-09-23 14:02:52 -0500

    Add Test to Vaccination Title Comp

---
+++ client/tests/unit/vaccination-title.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Buefy from 'buefy'
+import VaccinationTitle from '@/components/VaccinationTitle.vue'
+
+const localVue = createLocalVue()
+localVue.use(Buefy)
+const wrapper = shallowMount(VaccinationTitle, { localVue })
+
+describe('Vaccination Title Component', () => {
+	it('Should have a main div container', () => {
+		expect(wrapper.get('#vaccination-title')).toBeTruthy()
+	})
+
+	it('Sets the correct default data', () => {
+		expect(typeof VaccinationTitle.data).toBe('function')
+		const data = VaccinationTitle.data()
+		expect(data.title).toMatch('Vaccination and Deworming Record')
+		expect(data.labelButton).toMatch('Add')
+	})
+})
