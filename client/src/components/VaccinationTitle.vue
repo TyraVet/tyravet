4ce993d9a4fb00f65802f9f9dd7d4236c3a45b98
Andres Ruiz <andrsruiz@gmail.com>
2020-09-23 14:03:04 -0500

    Init Vaccination Title Comp

---
+++ client/src/components/VaccinationTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='vaccination-title'>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'VaccinationTitle',
+	data(){
+		return{
+			title: 'Vaccination and Deworming Record',
+			labelButton: 'Add'
+		}
+	}
+}
+</script>
