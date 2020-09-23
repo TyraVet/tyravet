3204c89087cd81e771a9e40f7a42ac0b1cc3e79e
Andres Ruiz <andrsruiz@gmail.com>
2020-09-23 14:03:36 -0500

    Init Test for Vaccination Record Comp

---
+++ client/tests/unit/vaccination-record.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VaccinationRecord from '@/components/VaccinationRecord.vue'
+import VaccinationTitle from '@/components/VaccinationTitle.vue'
+import VaccinationList from '@/components/VaccinationList.vue'
+
+const localVue = createLocalVue()
+const wrapper = shallowMount(VaccinationRecord, { localVue })
+
+describe('Vaccination Record Component', () => {
+	it('Should has components', () => {
+		expect(typeof VaccinationRecord.components).toBe('object')
+	})
+
+	it('Should have a main div container', () => {
+		expect(wrapper.get('#vaccination-record')).toBeTruthy()
+	})
+
+	it('Renders a VaccinationTitle Component', () => {
+		const vaccinationTitleComp = wrapper.findComponent(VaccinationTitle)
+		expect(vaccinationTitleComp.exists()).toBeTruthy()
+	})
+
+	it('Renders a VaccinationList Component', () => {
+		const vaccinationListComp = wrapper.findComponent(VaccinationList)
+		expect(vaccinationListComp.exists()).toBeTruthy()
+	})
+})
