bde065b8070583aa9374060bd37ccf927af630c1
Andres Ruiz <andrsruiz@gmail.com>
2020-09-23 14:03:15 -0500

    Init Test for Vaccination List Comp

---
+++ client/tests/unit/vaccination-list.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Buefy from 'buefy'
+import VaccinationList from '@/components/VaccinationList.vue'
+
+const localVue = createLocalVue()
+localVue.use(Buefy)
+const wrapper = shallowMount(VaccinationList, { localVue })
+
+describe('Vaccination List Component', () => {
+	it('Should have a main div container', () => {
+		expect(wrapper.get('#vaccination-list')).toBeTruthy()
+	})
+})
