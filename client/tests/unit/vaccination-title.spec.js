248437e00ca57f485bd1f58d8d5f57b85a301c28
Andres Ruiz <andrsruiz@gmail.com>
2020-09-24 17:21:36 -0500

    Test for title and button in VaccinationTitle Comp

--- client/tests/unit/vaccination-title.spec.js
+++ client/tests/unit/vaccination-title.spec.js
@@ -17,4 +17,23 @@ describe('Vaccination Title Component', () => {
 		expect(data.title).toMatch('Vaccination and Deworming Record')
 		expect(data.labelButton).toMatch('Add')
 	})
+
+	it('Should have an h1 tag as the Title', () => {
+		expect(wrapper.get('h1').exists()).toBeTruthy()
+		expect(wrapper.get('h1').classes('is-size-2')).toBeTruthy()
+		expect(wrapper.get('h1').text()).toMatch('Vaccination and Deworming Record')
+	})
+
+	it('Should have a button to Add Log', () => {
+		expect(wrapper.get('b-button-stub').exists()).toBeTruthy()
+		expect(wrapper.get('b-button-stub').attributes().nativetype).toMatch('button')
+		expect(wrapper.get('b-button-stub').attributes().tag).toMatch('button')
+		expect(wrapper.get('b-button-stub').attributes().title).toMatch('Add Vaccination/Deworming Log')
+		expect(wrapper.get('b-button-stub').attributes().type).toMatch('is-primary')
+		expect(wrapper.get('b-button-stub').attributes().size).toMatch('is-medium')
+		expect(wrapper.get('b-button-stub').attributes('has-icon')).toMatch('')
+		expect(wrapper.get('b-button-stub').attributes().iconpack).toMatch('fas')
+		expect(wrapper.get('b-button-stub').attributes().iconleft).toMatch('plus')
+		expect(wrapper.get('b-button-stub').text()).toMatch('Add')
+	})
 })
