d41eedccaaea474389cc404c752d9b8d4f753aea
Andres Ruiz <andrsruiz@gmail.com>
2020-09-24 16:12:28 -0500

    Init MedicalTitle Comp

---
+++ client/src/components/MedicalTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id='medical-title'>
+  </section>
+</template>
+
+<script>
+export default {
+	name: 'MedicalTitle',
+	data(){
+		return{
+			title: 'Medical Record'
+		}
+	}
+}
+</script>
