92358e0d219bea90c89fccdec99e214ee2ccb584
Andres Ruiz <andrsruiz@gmail.com>
2020-09-25 14:59:17 -0500

    Test for Icon

--- client/tests/unit/medical-title.spec.js
+++ client/tests/unit/medical-title.spec.js
@@ -1,7 +1,9 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Buefy from 'buefy'
 import MedicalTitle from '@/components/MedicalTitle.vue'
 
 const localVue = createLocalVue()
+localVue.use(Buefy)
 const wrapper = shallowMount(MedicalTitle, { localVue })
 
 describe('Medical Title Component', () => {
@@ -17,10 +19,19 @@ describe('Medical Title Component', () => {
 
 	const main = wrapper.get('#medical-title')
 
+	it('Should have an Stethoscope Icon', () => {
+		const icon = main.get('b-icon-stub')
+		expect(icon.exists()).toBeTruthy()
+		expect(icon.attributes().pack).toMatch('fas')
+		expect(icon.attributes().icon).toMatch('stethoscope')
+		expect(icon.attributes().size).toMatch('is-large')
+		expect(icon.attributes().type).toMatch('is-icon')
+	})
+
 	it('Renders the Title', () => {
 		const h1 = main.get('h1')
 		expect(h1.exists()).toBeTruthy()
-		expect(h1.attributes().class).toMatch('is-size-2')
+		expect(h1.attributes().class).toMatch('is-size-3')
 		expect(h1.text()).toMatch('Medical Record')
 	})
 })
