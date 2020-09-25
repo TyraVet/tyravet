340c189a28c5078a7d35ebc978f085b05cbf8999
Andres Ruiz <andrsruiz@gmail.com>
2020-09-25 16:51:09 -0500

    Set data and Error Message on MedicalList

--- client/src/components/MedicalList.vue
+++ client/src/components/MedicalList.vue
@@ -1,10 +1,30 @@
 <template>
   <section id='medical-list'>
+	<span id='no-records'>
+	  <b-message id='message-container'
+				 type='is-danger'
+				 icon-pack='fas'
+				 icon-size='is-large'
+				 icon='exclamation-circle'
+				 has-icon
+				 class='message'>
+		<h1 id='message'
+			class='has-text-danger'>
+		  {{ noRecords }}
+		</h1>
+	  </b-message>
+	</span>
   </section>
 </template>
 
 <script>
 export default {
-	name: 'MedicalList'
+	name: 'MedicalList',
+	data(){
+		return{
+			records: [],
+			noRecords: 'There are no Medical Records for this pet.'
+		}
+	}
 }
 </script>
