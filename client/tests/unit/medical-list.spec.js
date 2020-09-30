fa0ddf6398ca3d804458bf7eb2e2cc367e3a8cb5
Andres Ruiz <andrsruiz@gmail.com>
2020-09-30 11:11:57 -0500

    Update Test for MedicalList
    To Render an ErrorMessage Comp

--- client/tests/unit/medical-list.spec.js
+++ client/tests/unit/medical-list.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Buefy from 'buefy'
 import MedicalList from '@/components/MedicalList.vue'
+import ErrorMessage from '@/components/ErrorMessage.vue'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
@@ -13,7 +14,8 @@ describe('Medical List Component', () => {
 		expect(typeof MedicalList.data).toBe('function')
 		const data = MedicalList.data()
 		expect(data.records).toEqual(expect.arrayContaining([]))
-		expect(data.noRecords).toMatch('There are no Medical Records for this pet.')
+		expect(data.errorMessage).toMatch('There are no Medical Records for this pet.')
+		expect(data.noRecords).toBeTruthy()
 	})
 
 	it('Should has a section container', () => {
@@ -21,21 +23,9 @@ describe('Medical List Component', () => {
 	})
 
 	it('Shoudl has an error message if there are no records', () => {
-		const noRecords = main.get('#no-records')
-		expect(noRecords.exists()).toBeTruthy()
+		wrapper.setData({ noRecords: true })
 
-		const message = noRecords.get('#message-container')
-		expect(message.exists()).toBeTruthy()
-		expect(message.attributes().type).toMatch('is-danger')
-		expect(message.attributes().hasicon).toBeTruthy()
-		expect(message.attributes().iconpack).toMatch('fas')
-		expect(message.attributes().iconsize).toMatch('is-large')
-		expect(message.attributes().icon).toMatch('exclamation-circle')
-		expect(message.attributes().class).toMatch('message')
-
-		const messageText = message.get('#message')
-		expect(messageText.exists()).toBeTruthy()
-		expect(messageText.attributes().class).toMatch('has-text-danger')
-		expect(messageText.text()).toMatch('There are no Medical Records for this pet.')
+		const errorMessage = main.findComponent(ErrorMessage)
+		expect(errorMessage.exists()).toBeTruthy()
 	})
 })
