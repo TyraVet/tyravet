6c73880507e37a9f8604a264211e6b5e56bef437
Andres Ruiz <andrsruiz@gmail.com>
2020-09-30 10:59:31 -0500

    Create ErrorMessage Component

---
+++ client/src/components/ErrorMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id='error-message'
+		   class='has-background-danger-alpha'>
+	<b-icon pack='fas'
+			icon='exclamation-circle'
+			size='is-large'
+			type='is-danger'>
+	</b-icon>
+	<span id='message'>
+	  <h1 class='has-text-black is-size-5'>
+		{{ message }}
+	  </h1>
+	</span>
+  </section>
+</template>
+
+<script>
+export default {
+	name: 'ErrorMessage',
+	props: {
+		message: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style scoped>
+@import '../assets/css/error-message.css'
+</style>
