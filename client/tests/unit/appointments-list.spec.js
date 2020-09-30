c60175566bcfd84eb3f4305d47a9ea372466bf55
Andres Ruiz <andrsruiz@gmail.com>
2020-09-30 11:26:35 -0500

    Update Test for AppointmentsList
    To Render an ErrorMessage Comp

--- client/tests/unit/appointments-list.spec.js
+++ client/tests/unit/appointments-list.spec.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 import Buefy from 'buefy'
 import store from '@/store'
 import AppointmentsList from '@/components/AppointmentsList.vue'
+import ErrorMessage from '@/components/ErrorMessage.vue'
 
 /* We import createLocalVue to load Buefy and store */
 const localVue = createLocalVue()
@@ -11,12 +12,22 @@ localVue.use(Buefy)
 const wrapper = shallowMount(AppointmentsList, { store, localVue })
 
 describe('AppointmentsList Component', () => {
+	const main = wrapper.get('#appointment-list-container')
+
 	it('Set the correct default data', () => {
 		expect(typeof AppointmentsList.data).toBe('function')
-		const defaultData = AppointmentsList.data()
-		expect(defaultData.schedule).toMatchObject({})
-		expect(defaultData.hours).toEqual(expect.arrayContaining([]))
-		expect(defaultData.error).toBeFalsy()
-		expect(defaultData.errorMessage).toMatch('No Data Available')
+		const data = AppointmentsList.data()
+		expect(data.schedule).toMatchObject({})
+		expect(data.hours).toEqual(expect.arrayContaining([]))
+		expect(data.error).toBeFalsy()
+		expect(data.errorMessage).toMatch('No Data Available'),
+		expect(data.noData).toBeTruthy()
+	})
+
+	it('Should has an error message on noData', () => {
+		wrapper.setData({ noData: true })
+
+		const errorMessage = main.findComponent(ErrorMessage)
+		expect(errorMessage.exists()).toBeTruthy()
 	})
 })
