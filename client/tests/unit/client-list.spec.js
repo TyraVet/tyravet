a9861384c65009eaa592885e3b1f78478104f33f
Andres Ruiz <andrsruiz@gmail.com>
2020-09-30 11:18:09 -0500

    Update Test for ClientList
    To Render an ErrorMessage Comp

--- client/tests/unit/client-list.spec.js
+++ client/tests/unit/client-list.spec.js
@@ -2,6 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import store from '@/store'
 import Buefy from 'buefy'
 import ClientList from '@/components/ClientList.vue'
+import ErrorMessage from '@/components/ErrorMessage.vue'
 
 /* We import createLocalVue to load Buefy and store */
 const localVue = createLocalVue()
@@ -20,19 +21,18 @@ const wrapper = shallowMount(ClientList, { store, localVue })
 describe('ClientList Component', () => {
 	it('Set the correct default data', () => {
 		expect(typeof ClientList.data).toBe('function')
-		const defaultData = ClientList.data()
-		expect(defaultData.clients).toEqual(expect.arrayContaining([]))
-		expect(defaultData.isBordered).toBeTruthy()
-		expect(defaultData.hasMobileCards).toBeTruthy()
-		expect(defaultData.noClients).toMatch("You don't have Clients now. Add One!")
+		const data = ClientList.data()
+		expect(data.clients).toEqual(expect.arrayContaining([]))
+		expect(data.isBordered).toBeTruthy()
+		expect(data.hasMobileCards).toBeTruthy()
+		expect(data.errorMessage).toMatch("You don't have Clients now. Add One!")
+		expect(data.noClients).toBeTruthy()
 	})
 
 	it('Should have an Error Messages if There are no Clients', async () => {
-		const errorMessage = wrapper.get('b-message-stub')
-		expect(errorMessage.attributes().type).toMatch('is-danger')
-		expect(errorMessage.attributes().iconpack).toMatch('fas')
-		expect(errorMessage.attributes().iconsize).toMatch('is-large')
-		expect(errorMessage.attributes().icon).toMatch('exclamation-circle')
-		expect(errorMessage.attributes().class).toMatch('message')
+		wrapper.setData({ noClients: true })
+
+		const errorMessage = wrapper.findComponent(ErrorMessage)
+		expect(errorMessage.exists()).toBeTruthy()
 	})
 })
