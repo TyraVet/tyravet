6df4241a761b9247be929a6b808ca07de53063fc
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:33:40 -0500

    Create Services View

---
+++ client/src/views/Services.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id='services'>
+	<ServiceHeader></ServiceHeader>
+	<ServiceList></ServiceList>
+  </section>
+</template>
+
+<script>
+import ServiceHeader from '@/components/ServiceHeader.vue'
+import ServiceList from '@/components/ServiceList.vue'
+
+export default {
+	name: 'Services',
+	components: { ServiceHeader, ServiceList }
+}
+</script>
