ee76e05fcc48d252918be4e21906c448c842982a
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:31:24 -0500

    Create BreedHeader Comp

---
+++ client/src/components/BreedHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <section id='breed-header' class='title-container'>
+	<h3 class='is-size-4 has-text-primary-dark'>{{ title }}</h3>
+	<b-button type='is-primary'
+			  icon-pack='fas'
+			  icon-left='plus'
+			  @click='launchModal()'>
+	  {{ labelButton }}
+	</b-button>
+  </section>
+</template>
+
+<script>
+import BreedForm from '@/components/BreedForm.vue'
+
+export default {
+	name: 'BreedHeader',
+	data(){
+		return{
+			title: 'Breeds',
+			labelButton: 'Add'
+		}
+	},
+	methods: {
+		launchModal(){
+			this.$buefy.modal.open({
+				parent: this,
+				component: BreedForm,
+				hasModalCard: true,
+				trapFocus: true
+			})
+		}
+	}
+}
+</script>
+
+<style scoped>
+@import '../assets/css/titles.css'
+</style>
