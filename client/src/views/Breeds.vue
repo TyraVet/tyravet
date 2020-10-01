43bd6c20d16be348d24bae3828ee8c75358a0dca
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:31:47 -0500

    Add BreedHeader into Breeds view

--- client/src/views/Breeds.vue
+++ client/src/views/Breeds.vue
@@ -1,44 +1,16 @@
 <template>
-  <div class='breeds'>
-	<span class='title-container'>
-	  <h3 class='is-size-4 has-text-primary-dark'>{{ title }}</h3>
-	  <b-button type='is-primary'
-				icon-pack='fas'
-				icon-left='plus'
-				@click=launchModal()>
-		{{ labelButton }}
-	  </b-button>
-	</span>
+  <div id='breeds'>
+	<BreedHeader></BreedHeader>
 	<BreedList></BreedList>
   </div>
 </template>
 
 <script>
+import BreedHeader from '@/components/BreedHeader.vue'
 import BreedList from '@/components/BreedList.vue'
-import BreedForm from '@/components/BreedForm.vue'
 
 export default {
 	name: 'Breeds',
-	components: { BreedList },
-	data() {
-		return {
-			title: 'Breeds',
-			labelButton: 'Add'
-		}
-	},
-	methods: {
-		launchModal(){
-			this.$buefy.modal.open({
-				parent: this,
-				component: BreedForm,
-				hasModalCard: true,
-				trapFocus: true
-			})
-		}
-	}
+	components: { BreedHeader, BreedList }
 }
 </script>
-
-<style>
-@import '../assets/css/titles.css'
-</style>
