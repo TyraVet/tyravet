3e4959bf7b311f57dbf680016232cc929fd7244f
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:32:39 -0500

    Set new route for Services

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,7 @@ import Clients from '../views/Clients.vue'
 import Pets from '../views/Pets.vue'
 import Pet from '../views/Pet.vue'
 import Panel from '../views/Panel.vue'
+import Services from '../views/Services.vue'
 import Breeds from '../views/Breeds.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
@@ -51,6 +52,14 @@ const routes = [
 		component: Panel,
 		meta: {
 			title: 'TyraWeb Admin Panel'
+		}
+	},
+	{
+		path: '/panel/services',
+		name: 'services',
+		component: Services,
+		meta: {
+			title: 'TyraWeb Services List'
 		}
 	},
 	{
