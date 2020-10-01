9ec57baf3b02652234ade2e5c36bea27e8f5eb93
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:33:24 -0500

    Create ServiceHeader Comp

---
+++ client/src/components/ServiceHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id='service-header' class='title-container'>
+	<h3 class='is-size-4 has-text-primary-dark'>{{ title }}</h3>
+	<b-button type='is-primary'
+			  icon-pack='fas'
+			  icon-left='plus'
+			  @click='launchModal()'>
+	  {{ labelButton }}
+	</b-button>
+  </section>
+</template>
+
+<script>
+export default {
+	name: 'ServicedHeader',
+	data(){
+		return{
+			title: 'Services',
+			labelButton: 'Add'
+		}
+	},
+	methods: {
+		launchModal(){
+		}
+	}
+}
+</script>
+
+<style scoped>
+@import '../assets/css/titles.css'
+</style>
