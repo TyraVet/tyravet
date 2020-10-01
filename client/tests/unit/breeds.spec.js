a63a6b062a126be8651cd1ec4531c1b39bebb1f6
Andres Ruiz <andrsruiz@gmail.com>
2020-10-01 13:31:34 -0500

    Update Test for Breeds View
    Testing for child components

--- client/tests/unit/breeds.spec.js
+++ client/tests/unit/breeds.spec.js
@@ -1,25 +1,23 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Buefy from 'buefy'
 import Breeds from '@/views/Breeds.vue'
+import BreedHeader from '@/components/BreedHeader.vue'
 import BreedList from '@/components/BreedList.vue'
 
 /* We import createLocalVue to load Buefy and don't
  * have warnings due to its own components. */
-
 const localVue = createLocalVue()
-localVue.use(Buefy)
 const wrapper = shallowMount(Breeds, { localVue })
 
-describe('Breeds Component', () => {
-	it('Sets the correct default data', () => {
-		expect(typeof Breeds.data).toBe('function')
-		const defaultData = Breeds.data()
-		expect(defaultData.title).toMatch('Breeds')
-		expect(defaultData.labelButton).toMatch('Add')
+describe('Breeds View', () => {
+	const main = wrapper.get('#breeds')
+
+	it('Should has a BreedHeader Component', () => {
+		const breedHeaderComponent = main.findComponent(BreedHeader)
+		expect(breedHeaderComponent.exists()).toBeTruthy()
 	})
 
-	it('Should have a BreedList Component', () => {
-		const breedListComponent = wrapper.findComponent(BreedList)
+	it('Should has a BreedList Component', () => {
+		const breedListComponent = main.findComponent(BreedList)
 		expect(breedListComponent.exists()).toBeTruthy()
 	})
 })
