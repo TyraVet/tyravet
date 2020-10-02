1a3d5d5a53c5f4213f2ddad270d27108bd95f121
Andres Ruiz <andrsruiz@gmail.com>
2020-10-02 14:12:19 -0500

    Create TableEditDelete Comp

---
+++ client/src/components/TableEditDelete.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id='table-edit-delete'>
+	{{ text }}
+	<b-button id='edit'
+			  title='Edit'
+			  type='is-warning'
+			  icon-pack='fas'
+			  icon-left='edit'
+			  class='has-text-dark'></b-button>
+	<b-button id='delete'
+			  title='Delete'
+			  type='is-danger'
+			  icon-pack='fas'
+			  icon-left='trash-alt'
+			  class='has-text-white'></b-button>
+  </section>
+</template>
+
+<script>
+export default {
+	name: 'TableEditDelete',
+	props: {
+		id: {
+			type: String,
+			required: true
+		},
+		text: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style scoped>
+@import '../assets/css/table-edit-delete.css'
+</style>
