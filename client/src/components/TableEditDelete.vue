fc732ece8f84780256107d906384116d4108dca3
Andres Ruiz <andrsruiz@gmail.com>
2020-10-06 11:36:40 -0500

    Migrate html to pug

--- client/src/components/TableEditDelete.vue
+++ client/src/components/TableEditDelete.vue
@@ -1,23 +1,23 @@
-<template>
-  <section id='table-edit-delete'>
-	{{ text }}
-	<div id='button-container'>
-	  <b-button id='edit'
-				title='Edit'
-				type='is-warning'
-				icon-pack='fas'
-				icon-left='edit'
-				class='has-text-dark'
-				@click=edit()></b-button>
-	  <b-button id='delete'
-				title='Delete'
-				type='is-danger'
-				icon-pack='fas'
-				icon-left='trash-alt'
-				class='has-text-white'
-				@click=deleteAt()></b-button>
-	</div>
-  </section>
+<template lang='pug'>
+section#table-edit-delete
+	p {{ text }}
+	div#button-container
+		b-button#edit(
+			title='Edit'
+			type='is-warning'
+			icon-pack='fas'
+			icon-left='edit'
+			class='has-text-dark'
+			@click='edit()'
+		)
+		b-button#delete(
+			title='Delete'
+			type='is-danger'
+			icon-pack='fas'
+			icon-left='trash-alt'
+			class='has-text-white'
+			@click='deleteAt()'
+		)
 </template>
 
 <script lang='js'>
