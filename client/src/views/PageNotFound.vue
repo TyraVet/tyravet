6ae576d24fceb985c999d71230413c3e41421b9f
Andres Ruiz <andrsruiz@gmail.com>
2020-10-06 12:03:56 -0500

    Update to pug and set default data

--- client/src/views/PageNotFound.vue
+++ client/src/views/PageNotFound.vue
@@ -1,19 +1,23 @@
-<template>
-  <div class='page-not-found-container'>
-	<div class='page-not-found'>
-	  <h1 class='is-size-1'>Page Not Found</h1>
-	  <b-button title='Go Back'
-				type='is-primary'
-				@click=goBack()>
-		Go Back
-	  </b-button>
-	</div>
-  </div>
+<template lang='pug'>
+div#page-not-found-container
+	div#page-not-found
+		h1.is-size-1 {{ title }}
+		b-button(
+			title='Go Back'
+			type='is-primary'
+			@click='goBack()'
+		) {{ labelButton }}
 </template>
 
 <script lang='js'>
 export default {
 	name: 'PageNotFound',
+	data(){
+		return{
+			title: 'Page Not Found',
+			labelButton: 'Go Back'
+		}
+	},
 	methods: {
 		goBack(){
 			this.$router.go(-1)
