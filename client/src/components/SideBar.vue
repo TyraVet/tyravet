282fe4390c977aa9085b57a54703b8b27dd76e9a
Andres Ruiz <andrsruiz@gmail.com>
2020-10-07 12:07:05 -0500

    Set pug templating in SideBar

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -1,149 +1,155 @@
-<template>
-  <section id='side-bar'>
-	<b-sidebar type='is-primary-light'
-			   :can-cancel='canCancel'
-			   :fullheight='fullheight'
-			   :fullwidth='fullwidth'
-			   :open.sync='open'>
-	  <div id='menu-container'>
-		<b-menu>
-		  <b-menu-list label='Menu'>
-			<b-menu-item id='panel'
-						 label='Panel'
-						 icon-pack='fas'
-						 icon='database'
-						 v-if='isAdmin'>
-			  <span id='panel-home'
-					@click=changeSideBarState()>
-				<b-menu-item label='Home'
-							 icon-pack='fas'
-							 icon='home'
-							 tag='router-link'
-							 to='/panel'>
-				</b-menu-item>
-			  </span>
-			  <span id='panel-services'
-					@click=changeSideBarState()>
-				<b-menu-item label='Services'
-							 icon-pack='fas'
-							 icon='database'
-							 tag='router-link'
-							 to='/panel/services'>
-				</b-menu-item>
-			  </span>
-			  <span id='panel-breeds'
-					@click=changeSideBarState()>
-				<b-menu-item label='Breeds'
-							 icon-pack='fas'
-							 icon='database'
-							 tag='router-link'
-							 to='/panel/breeds'>
-				</b-menu-item>
-			  </span>
-			</b-menu-item>
-			<b-menu-item id='schedule'
-						 label='Schedule'
-						 icon-pack='fas'
-						 icon='calendar'>
-			  <span id='schedule-home'
-					@click=changeSideBarState()>
-				<b-menu-item label='Home'
-							 icon-pack='fas'
-							 icon='home'
-							 tag='router-link'
-							 to='/'>
-				</b-menu-item>
-			  </span>
-			  <span id='schedule-clients'
-					@click=changeSideBarState()>
-				<b-menu-item label='Clients Book'
-							 icon-pack='fas'
-							 icon='address-book'
-							 tag='router-link'
-							 to='/clients'>
-				</b-menu-item>
-			  </span>
-			  <span id='schedule-pets'
-					@click=changeSideBarState()>
-				<b-menu-item label='Pets Book'
-							 icon-pack='fas'
-							 icon='paw'
-							 tag='router-link'
-							 to='/pets'>
-				</b-menu-item>
-			  </span>
-			</b-menu-item>
-			<b-menu-item id='hospital'
-						 label='Hospital'
-						 icon-pack='fas'
-						 icon='hospital'>
-			  <span @click=changeSideBarState()>
-				<b-menu-item label='Home'
-							 icon-pack='fas'
-							 icon='home'
-							 tag='router-link'
-							 to='/hospital'>
-				</b-menu-item>
-			  </span>
-			</b-menu-item>
-			<b-menu-item id='inventory'
-						 label='Inventory'
-						 icon-pack='fas'
-						 icon='warehouse'>
-			  <span @click=changeSideBarState()>
-				<b-menu-item label='Home'
-							 icon-pack='fas'
-							 icon='home'
-							 tag='router-link'
-							 to='/inventory'>
-				</b-menu-item>
-			  </span>
-			</b-menu-item>
-		  </b-menu-list>
-		  <b-menu-list id='actions'
-					   label='Actions'>
-			<span @click=logOut()>
-			  <b-menu-item label='Log Out'
-						   icon-pack='fas'
-						   icon='sign-out-alt'>
-			  </b-menu-item>
-			</span>
-		  </b-menu-list>
-		  <b-menu-list id='about'
-					   label='About'>
-			<b-menu-item label='Contributing'
-						 icon-pack='fas'
-						 icon='beer'
-						 tag='a'
-						 :href='contribute'
-						 target='_blank'>
-			</b-menu-item>
-			<b-menu-item label='Found a Bug?'
-						 icon-pack='fas'
-						 icon='bug'
-						 tag='a'
-						 :href='bug'
-						 target='_blank'>
-			</b-menu-item>
-			<b-menu-item label='License'
-						 icon-pack='fas'
-						 icon='balance-scale'
-						 tag='a'
-						 :href='mit'
-						 target='_blank'>
-			</b-menu-item>
-		  </b-menu-list>
-		  <br>
-		  <b-button type='is-primary'
+<template lang='pug'>
+section#side-bar
+	b-sidebar(
+		type='is-primary-light'
+		:can-cancel='canCancel'
+		:fullheight='fullheight'
+		:fullwidth='fullwidth'
+		:open.sync='open'
+	)
+		div#menu-container
+			b-menu
+				b-menu-list( label='Menu' )
+					b-menu-item#panel(
+						label='Panel'
+						icon-pack='fas'
+						icon='database'
+						v-if='isAdmin'
+					)
+						span#panel-home(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Home'
+								icon-pack='fas'
+								icon='home'
+								tag='router-link'
+								to='/panel'
+							)
+						span#panel-services(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Services'
+								icon-pack='fas'
+								icon='database'
+								tag='router-link'
+								to='/panel/services'
+							)
+						span#panel-breeds(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Breeds'
+								icon-pack='fas'
+								icon='database'
+								tag='router-link'
+								to='/panel/breeds'
+							)
+					b-menu-item#schedule(
+						label='Schedule'
+						icon-pack='fas'
+						icon='calendar'
+					)
+						span#schedule-home(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Home'
+								icon-pack='fas'
+								icon='home'
+								tag='router-link'
+								to='/'
+							)
+						span#schedule-clients(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Clients Book'
+								icon-pack='fas'
+								icon='address-book'
+								tag='router-link'
+								to='/clients'
+							)
+						span#schedule-pets(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Pets Book'
+								icon-pack='fas'
+								icon='paw'
+								tag='router-link'
+								to='/pets'
+							)
+					b-menu-item#hospital(
+						label='Hospital'
+						icon-pack='fas'
+						icon='hospital'
+					)
+						span(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Home'
+								icon-pack='fas'
+								icon='home'
+								tag='router-link'
+								to='/hospital'
+							)
+					b-menu-item#inventory(
+						label='Inventory'
+						icon-pack='fas'
+						icon='warehouse'
+					)
+						span(
+							@click='changeSideBarState()'
+						)
+							b-menu-item(
+								label='Home'
+								icon-pack='fas'
+								icon='home'
+								tag='router-link'
+								to='/inventory'
+							)
+				b-menu-list#actions( label='Actions' )
+					span( @click='logOut()' )
+						b-menu-item(
+							label='Log Out'
+							icon-pack='fas'
+							icon='sign-out-alt'
+						)
+				b-menu-list#about( label='About' )
+					b-menu-item(
+						label='Contributing'
+						icon-pack='fas'
+						icon='beer'
+						tag='a'
+						:href='contribute'
+						target='_blank'
+					)
+					b-menu-item(
+						label='Found a Bug?'
+						icon-pack='fas'
+						icon='bug'
+						tag='a'
+						:href='bug'
+						target='_blank'
+					)
+					b-menu-item(
+						label='License'
+						icon-pack='fas'
+						icon='balance-scale'
+						tag='a'
+						:href='mit'
+						target='_blank'
+					)
+				br
+				b-button(
+					type='is-primary'
 					icon-pack='fas'
 					icon-left='times'
-					@click=changeSideBarState()>
-			Close
-		  </b-button>
-		</b-menu>
-	  </div>
-	</b-sidebar>
-  </section>
+					@click='changeSideBarState()'
+				) Close
 </template>
 
 <script lang='js'>
