01a532e9ea66afd2c6ff80d2d4effc7dfd3990a8
Andres Ruiz <andrsruiz@gmail.com>
2020-10-07 13:33:24 -0500

    Update ServiceForm Layout

--- client/src/components/ServiceForm.vue
+++ client/src/components/ServiceForm.vue
@@ -1,5 +1,43 @@
 <template lang='pug'>
-section#service-form
+form#service-form
+	div.modal-card( style='width: auto' )
+		header.modal-card-head
+			p.modal-card-title {{ title }}
+			button.delete(
+				type='button'
+				@click='close()'
+			)
+		section.modal-card-body
+			b-field( label='Name' )
+			b-input(
+				type='text'
+				v-model='serviceName',
+				required
+			)
+		footer.modal-card-foot
+			button.button(
+				type='button'
+				@click='close()'
+			) {{ labelButtonCancel }}
+			b-button.button.is-success(
+				@click='send()'
+			) {{ labelButtonAccept }}
+			b-icon#success-icon(
+				title='Success'
+				type='is-success'
+				pack='fas'
+				size='is-large'
+				icon='check'
+				v-if='status === 201 || status === 200'
+			)
+			b-icon#error-icon(
+				title='Error'
+				type='is-danger'
+				pack='fas'
+				size='is-large'
+				icon='exclamation'
+				v-if='status === 401 || status === 404'
+			)
 </template>
 
 <script lang='js'>
@@ -14,8 +52,12 @@ export default {
 	},
 	data(){
 		return{
+			title: '',
 			serviceName: '',
-			servicePrice: 0
+			servicePrice: 0,
+			labelButtonCancel: 'Cancel',
+			labelButtonAccept: 'Accept',
+			status: 0
 		}
 	}
 }
