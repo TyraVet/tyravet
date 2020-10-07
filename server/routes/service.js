8d8a9c1cbde1e1209a7f388f00e50711dacd1867
Andres Ruiz <andrsruiz@gmail.com>
2020-10-07 16:49:23 -0500

    Set route for Update Service Method

--- server/routes/service.js
+++ server/routes/service.js
@@ -9,5 +9,6 @@ var service = require(path.join(__dirname, '../controllers/service.js'))
 /* Service Routes */
 router.get('/', authenticateToken, service.get_services)
 router.get('/service', authenticateToken, service.get_service)
+router.post('/update', authenticateToken, service.update_service)
 
 module.exports = router
