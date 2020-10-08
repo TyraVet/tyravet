7bc707da655bfdf2511d75400cb0719d4c0868dc
Andres Ruiz <andrsruiz@gmail.com>
2020-10-08 13:57:25 -0500

    Set route to delete service method

--- server/routes/service.js
+++ server/routes/service.js
@@ -11,5 +11,6 @@ router.get('/', authenticateToken, service.get_services)
 router.post('/create', authenticateToken, service.post_create_service)
 router.get('/service', authenticateToken, service.get_service)
 router.post('/update', authenticateToken, service.update_service)
+router.get('/delete', authenticateToken, service.delete_service)
 
 module.exports = router
