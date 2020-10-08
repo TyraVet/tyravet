01c01c2bfe57e321009295491e548fdeaf5f0e8d
Andres Ruiz <andrsruiz@gmail.com>
2020-10-08 14:07:39 -0500

    Test for icon showing on status changed

--- client/tests/unit/delete-confirmation.spec.js
+++ client/tests/unit/delete-confirmation.spec.js
@@ -24,6 +24,7 @@ describe('Delete Confirmation Component', () => {
 	const data = DeleteConfirmation.data()
 	it('Should set the default data', () => {
 		expect(typeof DeleteConfirmation.data).toBe('function')
+		expect(data.status).toBe(0)
 		expect(data.title).toMatch('Delete Confirmation')
 		expect(data.danger).toMatch('Are you sure you want to delete this item? Proceed with caution.')
 		expect(data.labelButtonCancel).toMatch('Cancel')
@@ -71,7 +72,7 @@ describe('Delete Confirmation Component', () => {
 	})
 
 	const modalFooter = modal.get('.modal-card-foot')
-	it('Should has a modal Foot', () => {
+	it('Should has a modal Foot', async () => {
 		expect(modalFooter.exists()).toBeTruthy()
 
 		const cancelButton = modalFooter.get('button')
@@ -83,5 +84,43 @@ describe('Delete Confirmation Component', () => {
 		expect(acceptButton.exists()).toBeTruthy()
 		expect(acceptButton.attributes().title).toMatch(data.labelButtonAccept)
 		expect(acceptButton.text()).toMatch(data.labelButtonAccept)
+
+		await wrapper.setData({ status: 200 })
+		const successIcon = modalFooter.get('#success-icon')
+		expect(successIcon.exists()).toBeTruthy()
+		expect(successIcon.attributes().title).toMatch('Success')
+		expect(successIcon.attributes().type).toMatch('is-success')
+		expect(successIcon.attributes().pack).toMatch('fas')
+		expect(successIcon.attributes().size).toMatch('is-large')
+		expect(successIcon.attributes().icon).toMatch('check')
+
+		await wrapper.setData({ status: 401 })
+		const errorIcon = modalFooter.get('#error-icon')
+		expect(errorIcon.exists()).toBeTruthy()
+		expect(errorIcon.attributes().title).toMatch('Error')
+		expect(errorIcon.attributes().type).toMatch('is-danger')
+		expect(errorIcon.attributes().pack).toMatch('fas')
+		expect(errorIcon.attributes().size).toMatch('is-large')
+		expect(errorIcon.attributes().icon).toMatch('exclamation')
+	})
+
+	it('Should has a close method', () => {
+		expect(wrapper.vm.close).toBeTruthy()
+	})
+
+	it('Should has a send method', () => {
+		expect(wrapper.vm.send).toBeTruthy()
+	})
+
+	it('Should has a setOnSuccess method', () => {
+		expect(wrapper.vm.setOnSuccess).toBeTruthy()
+	})
+
+	it('Should has a setOnError method', () => {
+		expect(wrapper.vm.setOnError).toBeTruthy()
+	})
+
+	it('Should has a deleteService method', () => {
+		expect(wrapper.vm.deleteService).toBeTruthy()
 	})
 })
