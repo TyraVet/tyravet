6754007872c31bc96a1e4d16a1cae0b0b13995b4
Andres Ruiz <andrsruiz@gmail.com>
2020-10-12 12:59:29 -0500

    Set as get method to find user

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,12 +4,12 @@ const path = require('path')
 const authenticateToken = require('../middlewares/authenticateToken.js')
 
 /* Require Controller Modules */
-var userController = require(path.join(__dirname, '../controllers/user.js'))
+var user = require(path.join(__dirname, '../controllers/user.js'))
 
 /* User Routes */
-router.post('/find', authenticateToken, userController.post_find_user_by_id)
-router.get('/login', userController.get_login)
-router.post('/signup', authenticateToken, userController.post_signup)
-router.get('/', authenticateToken, userController.get_users)
+router.post('/find', authenticateToken, user.get_user)
+router.get('/login', user.get_login)
+router.post('/signup', authenticateToken, user.post_signup)
+router.get('/', authenticateToken, user.get_users)
 
 module.exports = router
