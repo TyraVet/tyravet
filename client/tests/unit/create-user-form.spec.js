ed27864045529b7f976f5ee125ff2720fdb9e861
Andres Ruiz <andrsruiz@gmail.com>
2020-10-12 11:53:02 -0500

    Test for correct layout of component

--- client/tests/unit/create-user-form.spec.js
+++ client/tests/unit/create-user-form.spec.js
@@ -18,28 +18,92 @@ store.state.user = {
 const wrapper = shallowMount(CreateUserForm, { store, localVue })
 
 describe('CreateUserForm Component', () => {
-	const main = wrapper.get('#create-user-form')
-
-	const modal = main.get('div')
-
-	const modalHeader = modal.get('.modal-card-head')
-
-	const modalBody = modal.get('.modal-card-body')
-
-	const modalFooter = modal.get('.modal-card-foot')
-
+	const data = CreateUserForm.data()
 	it('Set the correct default data', () => {
 		expect(typeof CreateUserForm.data).toBe('function')
-		const defaultData = CreateUserForm.data()
-		expect(defaultData.title).toMatch('')
-		expect(defaultData.username).toMatch('')
-		expect(defaultData.password).toMatch('')
-		expect(defaultData.confirmPassword).toMatch('')
-		expect(defaultData.validationMessageConfirmPassword).toMatch('')
-		expect(defaultData.status).toBeNull()
-		expect(defaultData.statusText).toMatch('')
-		expect(defaultData.error).toMatch('')
-		expect(defaultData.type).toMatch('medic')
+		expect(data.title).toMatch('')
+		expect(data.username).toMatch('')
+		expect(data.password).toMatch('')
+		expect(data.confirmPassword).toMatch('')
+		expect(data.validationMessageConfirmPassword).toMatch('')
+		expect(data.status).toBeNull()
+		expect(data.statusText).toMatch('')
+		expect(data.error).toMatch('')
+		expect(data.type).toMatch('medic')
+	})
+
+	const main = wrapper.get('#create-user-form')
+	it('Should has a main container', () => {
+		expect(main.exists()).toBeTruthy()
+	})
+
+	const modal = main.get('div')
+	it('Should has a modal', () => {
+		expect(modal.exists()).toBeTruthy()
+	})
+
+	const modalHeader = modal.get('.modal-card-head')
+	it('Should has a modal Header', () => {
+		expect(modalHeader.exists()).toBeTruthy()
+
+		const title = modalHeader.get('.modal-card-title')
+		expect(title.exists()).toBeTruthy()
+		expect(title.text()).toMatch(data.title)
+
+		const button = modalHeader.get('button')
+		expect(button.exists()).toBeTruthy()
+		expect(button.attributes().type).toMatch('button')
+	})
+
+	const modalBody = modal.get('.modal-card-body')
+	it('Should has a modal Body', () => {
+		expect(modalBody.exists()).toBeTruthy()
+
+		const usernameLabel = modalBody.get('#username')
+		expect(usernameLabel.exists()).toBeTruthy()
+		expect(usernameLabel.attributes().label).toMatch('Username')
+
+		const usernameInput = usernameLabel.get('b-input-stub')
+		expect(usernameInput.exists()).toBeTruthy()
+		expect(usernameInput.attributes().type).toMatch('text')
+		expect(usernameInput.attributes().minlength).toMatch('4')
+		expect(usernameInput.attributes().required).toBeTruthy()
+
+		const passwordLabel = modalBody.get('#password')
+		expect(passwordLabel.exists()).toBeTruthy()
+		expect(passwordLabel.attributes().label).toMatch('Password')
+
+		const passwordInput = passwordLabel.get('b-input-stub')
+		expect(passwordInput.exists()).toBeTruthy()
+		expect(passwordInput.attributes().type).toMatch('password')
+		expect(passwordInput.attributes().minlength).toMatch('8')
+		expect(passwordInput.attributes().required).toBeTruthy()
+
+		const confirmPassLabel = modalBody.get('#confirm-password')
+		expect(confirmPassLabel.exists()).toBeTruthy()
+		expect(confirmPassLabel.attributes().label).toMatch('Confirm Password')
+
+		const confirmPassInput = confirmPassLabel.get('b-input-stub')
+		expect(confirmPassInput.exists()).toBeTruthy()
+		expect(confirmPassInput.attributes().type).toMatch('password')
+		expect(confirmPassInput.attributes().minlength).toMatch('8')
+		expect(confirmPassInput.attributes().required).toBeTruthy()
+	})
+
+	const modalFooter = modal.get('.modal-card-foot')
+	it('Should has a modal Footer', () => {
+		expect(modalFooter.exists()).toBeTruthy()
+
+		const cancelButton = modalFooter.get('button')
+		expect(cancelButton.exists()).toBeTruthy()
+		expect(cancelButton.attributes().type).toMatch('button')
+		expect(cancelButton.text()).toMatch('Cancel')
+
+		const acceptButton = modalFooter.get('b-button-stub')
+		expect(acceptButton.exists()).toBeTruthy()
+		expect(acceptButton.classes('button')).toBeTruthy()
+		expect(acceptButton.classes('is-success')).toBeTruthy()
+		expect(acceptButton.text()).toMatch('Accept')
 	})
 
 	it('Should have a Success Messages on Create Success Status', async () => {
