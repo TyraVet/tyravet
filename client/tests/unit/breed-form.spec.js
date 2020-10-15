9bfa42b26fd9715793b647d33f4db0f786958963
Andres Ruiz <andrsruiz@gmail.com>
2020-10-15 12:38:18 -0500

    Test layout of Breed Form

--- client/tests/unit/breed-form.spec.js
+++ client/tests/unit/breed-form.spec.js
@@ -10,12 +10,70 @@ localVue.use(Buefy)
 const wrapper = shallowMount(BreedForm, { store, localVue })
 
 describe('BreedForm Component', () => {
+	const main = wrapper.get('#breed-form')
+	it('Should has a main container', () => {
+		expect(main.exists()).toBeTruthy()
+	})
+
 	const data = BreedForm.data()
 	it('Set the correct default data', () => {
 		expect(typeof BreedForm.data).toBe('function')
 		expect(data.title).toMatch('Create Breed')
 		expect(data.breedName).toMatch('')
 		expect(data.status).toBeNull()
+		expect(data.labelButtonCancel).toMatch('Cancel')
+		expect(data.labelButtonAccept).toMatch('Accept')
+	})
+
+	const props = wrapper.props()
+	it('Should set default props if not sent', () => {
+		expect(props).toBeTruthy()
+		expect(props.breedId).toBeNull()
+	})
+
+	const modal = main.get('div')
+	it('Should has a modal component', () => {
+		expect(modal.exists()).toBeTruthy()
+		expect(modal.classes('modal-card')).toBeTruthy()
+	})
+
+	const modalHeader = modal.get('.modal-card-head')
+	it('Should has a modal Head', () => {
+		expect(modalHeader.exists()).toBeTruthy()
+
+		const title = modalHeader.get('.modal-card-title')
+		expect(title.exists()).toBeTruthy()
+		expect(title.text()).toMatch(data.title)
+
+		const closeButton = modalHeader.get('button')
+		expect(closeButton.exists()).toBeTruthy()
+	})
+
+	const modalBody = modal.get('.modal-card-body')
+	it('Should has a modal Body', () => {
+		expect(modalBody.exists()).toBeTruthy()
+
+		const field = modalBody.get('b-field-stub')
+		expect(field.exists()).toBeTruthy()
+		expect(field.attributes().label).toMatch('Name')
+
+		const input = modalBody.get('b-input-stub')
+		expect(input.exists()).toBeTruthy()
+		expect(input.attributes().type).toMatch('text')
+		expect(input.attributes().required).toBeTruthy()
+	})
+
+	const modalFooter = modal.get('.modal-card-foot')
+	it('Should has a modal Foot', () => {
+		expect(modalFooter.exists()).toBeTruthy()
+
+		const cancelButton = modalFooter.get('button')
+		expect(cancelButton.exists()).toBeTruthy()
+		expect(cancelButton.text()).toMatch(data.labelButtonCancel)
+
+		const acceptButton = modalFooter.get('b-button-stub')
+		expect(acceptButton.exists()).toBeTruthy()
+		expect(acceptButton.text()).toMatch(data.labelButtonAccept)
 	})
 
 	it('Should have a Success Messages on Create Success Status', async () => {
