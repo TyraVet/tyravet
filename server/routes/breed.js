d9ae8a66156be50b942bccf92de58edef5c0c991
Andres Ruiz <andrsruiz@gmail.com>
2020-10-15 13:13:14 -0500

    Set route for update Breed method

--- server/routes/breed.js
+++ server/routes/breed.js
@@ -10,5 +10,6 @@ var breed = require(path.join(__dirname, '../controllers/breed.js'))
 router.post('/create', authenticateToken, breed.post_create_breed)
 router.get('/', authenticateToken, breed.get_breeds)
 router.get('/find', authenticateToken, breed.get_breed)
+router.post('/update', authenticateToken, breed.update)
 
 module.exports = router
