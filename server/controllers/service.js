9dcbdfb2292f80914ab2e7c6cc13ec1e4d03190a
Andres Ruiz <andrsruiz@gmail.com>
2020-10-20 17:01:59 -0500

    Set code conventions in service

--- server/controllers/service.js
+++ server/controllers/service.js
@@ -1,65 +1,75 @@
-const Service = require('../models/service.js')
+const SERVICE = require('../models/service.js');
 
-/* Get all Services */
-exports.get_services = (req, res) => {
-	Service.find()
+/* Get all Services
+ *
+ * The user may want to see a list of all available services. */
+exports.getServices = (req, res) => {
+	SERVICE.find()
 		   .populate('service')
 		   .exec((err, services) => {
 			   if(err)
-				   return res.status(406).json(err)
+				   return res.status(406).json(err);
 
 			   /* Succes */
-			   res.status(200).json(services)
-		   })
-}
+			   res.status(200).json(services);
+		   });
+};
 
-/* Get one Service */
-exports.get_service = (req, res) => {
-	Service.findById(req.query.id, (err, theService) => {
+/* Get one Service
+ *
+ * The user may want to get just one Servie to See, Edit or Delete it. */
+exports.getService = (req, res) => {
+	SERVICE.findById(req.query.id, (err, service) => {
 		if(err)
-			return res.status(404).json(err)
+			return res.status(404).json(err);
 
 		/* Success */
-		res.status(200).json(theService)
-	})
-}
+		res.status(200).json(service);
+	});
+};
 
-/* Update Service */
-exports.update_service = (req, res) => {
-	Service.findByIdAndUpdate(req.body.id, {
+/* Update Service
+ *
+ * The user can update any service. */
+exports.update = (req, res) => {
+	SERVICE.findByIdAndUpdate(req.body.id, {
 		_id: req.body.id,
 		name: req.body.name,
 		price: req.body.price
 	}, (err) => {
 		if(err)
-			return res.status(406).json(err)
+			return res.status(406).json(err);
 
 		/* Success */
-		res.sendStatus(201)
-	})
-}
+		res.sendStatus(201);
+	});
+};
 
-/* Create Service */
-exports.post_create_service = (req, res) => {
-	const service = new Service({
+/* Create Service
+ *
+ * The user can create services. */
+exports.createService = (req, res) => {
+	const Service = new SERVICE({
 		name: req.body.name,
 		price: req.body.price
 	}).save(err => {
 		if(err)
-			return res.status(406).json(err)
+			return res.status(406).json(err);
 
 		/* Success */
-		res.sendStatus(201)
-	})
-}
+		res.sendStatus(201);
+	});
+};
 
-/* Delete Service */
-exports.delete_service = (req, res) => {
-	Service.findByIdAndRemove(req.query.id, (err) => {
+/* Delete Service
+ *
+ * The user can delete any service. */
+exports.delete = (req, res) => {
+	SERVICE.findByIdAndRemove(req.query.id, (err) => {
 		if(err)
-			res.status(406).json(err)
+			res.status(406).json(err);
 
 		/* Success */
-		res.sendStatus(200)
-	})
-}
+		res.sendStatus(200);
+	});
+};
