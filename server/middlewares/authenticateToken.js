b757c373c9d31d4858a0d4097c88bc4cb49fe098
Andres Ruiz <andrsruiz@gmail.com>
2020-10-20 13:46:13 -0500

    Set code convention for auth middleware

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken')
+const JWT = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
+	const AUTH_HEADER = req.headers['authorization'];
+	const TOKEN = AUTH_HEADER && AUTH_HEADER.split(' ')[1];
 
-	if(token == null)
-		return res.sendStatus(401)
+	if(TOKEN == null)
+		return res.sendStatus(401);
 
 	/* Success */
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+	JWT.verify(TOKEN, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
 		if(err)
-			return res.sendStatus(403)
+			return res.sendStatus(403);
 
 		/* Success */
-		req.user = user
-		next()
-	})
-}
+		req.user = user;
+		next();
+	});
+};
 
-module.exports = authenticateToken
+module.exports = authenticateToken;
