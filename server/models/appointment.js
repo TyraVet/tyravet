449686dc0bec8ba8cef6566af09290d6c156eac6
Andres Ruiz <andrsruiz@gmail.com>
2020-10-23 11:49:28 -0500

    Set camelCase to attributeas

--- server/models/appointment.js
+++ server/models/appointment.js
@@ -5,8 +5,8 @@ var Schema = MONGOOSE.Schema;
 
 const AppointmentSchema = new Schema({
 	service: { type: SERVICE, required: true },
-	client_id: { type: String, required: true },
-	pet_id: { type: String, required: true },
+	clientId: { type: String, required: true },
+	petId: { type: String, required: true },
 	hour: { type: String, required: true },
 	done: { type: Boolean, default: false }
 });
