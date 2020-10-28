b4aa44a649c4bca85dfc539ed250effd3c02a99c
Andres Ruiz <andrsruiz@gmail.com>
2020-10-28 11:37:02 -0600

    Set route to update vaccination records

--- server/routes/pet.js
+++ server/routes/pet.js
@@ -9,6 +9,7 @@ const PET = require(PATH.join(__dirname, '../controllers/pet.js'));
 /* Pet Routes */
 ROUTER.get('/', AUTH_TOKEN, PET.GetPet); /* Should be /find */
 ROUTER.get('/all', AUTH_TOKEN, PET.GetPets); /* Should be / */
+ROUTER.post('/add-vaccination-record', AUTH_TOKEN, PET.AddVaccinationRecord);
 ROUTER.post('/upload-picture', AUTH_TOKEN, PET.UploadProfilePicture);
 ROUTER.get('/picture', AUTH_TOKEN, PET.GetProfilePicture);
 
