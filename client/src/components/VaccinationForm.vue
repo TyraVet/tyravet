0abc9e954bafe4f41be5b592b9dc483241b9c630
Andres Ruiz <andrsruiz@gmail.com>
2020-10-28 17:42:26 -0600

    Send record to api, bug: the dates are wrong

--- client/src/components/VaccinationForm.vue
+++ client/src/components/VaccinationForm.vue
@@ -22,8 +22,8 @@ form#vaccination-form
 				option(
 					v-for='(shot, index) in services'
 					:key='index'
-					:value='shot'
-				) {{ service.name }}
+					:value='shot.name'
+				) {{ shot.name }}
 			b-field#vaccination-label-next-date(
 				label='Next Application Date'
 			)
@@ -47,6 +47,7 @@ form#vaccination-form
 				pack='fas'
 				size='is-large'
 				icon='check'
+				v-if='status === CREATED'
 			)
 			b-icon#error-icon(
 				title='Error'
@@ -54,11 +55,18 @@ form#vaccination-form
 				pack='fas'
 				size='is-large'
 				icon='exclamation'
+				v-if='status === AUTH || status === NOT_FOUND || status === ERROR'
 			)
 </template>
 
 <script lang='js'>
 import moment from 'moment'
+import axios from 'axios'
+
+export const CREATED = 201
+export const AUTH = 401
+export const NOT_FOUND = 404
+export const ERROR = 406
 
 export default {
 	name: 'VaccinationForm',
@@ -66,25 +74,65 @@ export default {
 		petId: {
 			type: String,
 			required: true
+		},
+		records: {
+			type: Array,
+			required: true
 		}
 	},
 	data(){
 		return{
+			CREATED,
+			AUTH,
+			NOT_FOUND,
+			ERROR,
 			title: 'Add Record',
 			labelButtonCancel: 'Cancel',
 			labelButtonAccept: 'Accept',
 			minDate: new Date(),
 			recordApplicationDate: moment(this.minDate).format('YYYY-MM-DD'),
 			recordShot: '',
-			recordMedic: '',
 			recordNextDate: new Date(),
-			services: null
+			services: null,
+			status: null
 		}
 	},
 	methods: {
-		init(){},
+		/* Init method to fetch from the API the shots. */
+		init(){ this.getShots() },
+		/* Close the modal through emitting the event. */
 		close(){ this.$emit('close') },
-		send(){}
+		/* POST request to store the vaccination record in the pet. */
+		send(){
+			axios.post(process.env.VUE_APP_TYRAWEB_PET_ADD_VACCINATION_RECORD, {
+				id: this.petId,
+				applicationDate: this.$store.state.today,
+				shot: this.recordShot,
+				medic: this.$store.state.user.username,
+				nextApplicationDate: moment(this.recordNextDate).format('YYYY-MM-DD'),
+				vaccinationRecords: this.records
+			}, {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.user.token
+				}
+			}).then(response => {
+				console.log(response)
+			}).catch(error => {
+				console.error(error)
+			})
+		},
+		/* GET request to get the vaccination services (shots). */
+		getShots(){
+			axios.get(process.env.VUE_APP_TYRAWEB_VACCINATION_SERVICES, {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.user.token
+				}
+			}).then(response => {
+				this.services = response.data
+			}).catch(error => {
+				console.error(error)
+			})
+		}
 	},
 	created(){
 		this.init()
