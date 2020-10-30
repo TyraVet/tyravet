ca9b8273b9762e72753fbbfeb2757d86b477e974
Andres Ruiz <andrsruiz@gmail.com>
2020-10-30 12:38:47 -0600

    Move api into own repo

--- server/models/type.js
+++
@@ -1,9 +0,0 @@
-const MONGOOSE = require('mongoose');
-
-var Schema = MONGOOSE.Schema;
-
-var TypeSchema = new Schema({
-	name: { type: String, require: true }
-});
-
-module.exports = MONGOOSE.model('Type', TypeSchema);
