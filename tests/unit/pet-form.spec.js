cdb5834aa0aa38a06c28093d091bae0373669c46
Andres Ruiz <andrsruiz@gmail.com>
2020-11-02 17:34:25 -0600

    Test for new data

--- tests/unit/pet-form.spec.js
+++ tests/unit/pet-form.spec.js
@@ -1,10 +1,10 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import store from '@/store'
-import Buefy from 'buefy'
-import PetForm from '@/components/PetForm.vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import store from '@/store';
+import Buefy from 'buefy';
+import PetForm from '@/components/PetForm.vue';
 
-const localVue = createLocalVue()
-localVue.use(Buefy)
+const localVue = createLocalVue();
+localVue.use(Buefy);
 
 /* Dummy User */
 store.state.user = {
@@ -12,20 +12,26 @@ store.state.user = {
 	username: '',
 	type: {},
 	token: ''
-}
+};
 
-const wrapper = shallowMount(PetForm, { store, localVue })
+const wrapper = shallowMount(PetForm, { store, localVue });
 
 describe('Pet Form', () => {
+	const data = PetForm.data();
 	it('Shoudl set the correct default data', () => {
-		expect(typeof PetForm.data).toBe('function')
-		const defaultData = PetForm.data()
-		expect(defaultData.title).toMatch('Add Pet')
-		expect(defaultData.breeds).toEqual(expect.arrayContaining([]))
-		expect(defaultData.petName).toMatch('')
-		expect(defaultData.petAge).toBeNull()
-		expect(defaultData.petWeight).toBeNull()
-		expect(defaultData.petBirthday).toBeNull()
-		expect(defaultData.petBreed).toMatch('')
-	})
-})
+		expect(typeof PetForm.data).toBe('function');
+		expect(data.OK).toBe(201);
+		expect(data.ERROR).toBe(403);
+		expect(data.SERVER_ERROR).toBe(500);
+		expect(data.FEMALE).toMatch('Female');
+		expect(data.MALE).toMatch('Male');
+		expect(data.title).toMatch('Add Pet');
+		expect(data.breeds).toEqual(expect.arrayContaining([]));
+		expect(data.petName).toMatch('');
+		expect(data.petAge).toBeNull();
+		expect(data.petWeight).toBeNull();
+		expect(data.petBirthday).toBeNull();
+		expect(data.petBreed).toMatch('');
+		expect(data.femaleOrMale).toBeFalsy();
+	});
+});
