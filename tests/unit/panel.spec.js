cf87baab52b0f0bdeae0a972e5e5a7b8e93e6b09
Andres Ruiz <andrsruiz@gmail.com>
2020-11-04 11:51:03 -0600

    Test for a configuration component in panel

--- tests/unit/panel.spec.js
+++ tests/unit/panel.spec.js
@@ -1,19 +1,25 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Panel from '@/views/Panel.vue'
-import UsersHeader from '@/components/UsersHeader.vue'
-import UsersList from '@/components/UsersList.vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Panel from '@/views/Panel.vue';
+import UsersHeader from '@/components/UsersHeader.vue';
+import UsersList from '@/components/UsersList.vue';
+import Configuration from '@/components/Configuration.vue';
 
-const localVue = createLocalVue()
-const wrapper = shallowMount(Panel, { localVue })
+const localVue = createLocalVue();
+const wrapper = shallowMount(Panel, { localVue });
 
 describe('Panel Component', () => {
-	it('Should have a UsersHeader Component', () => {
-		const usersHeaderComponent = wrapper.findComponent(UsersHeader)
-		expect(usersHeaderComponent.exists()).toBeTruthy()
-	})
+	it('Should has a UsersHeader Component', () => {
+		const usersHeaderComponent = wrapper.findComponent(UsersHeader);
+		expect(usersHeaderComponent.exists()).toBeTruthy();
+	});
 
-	it('Should have a UsersList Component', () => {
-		const usersListComponent = wrapper.findComponent(UsersList)
-		expect(usersListComponent.exists()).toBeTruthy()
-	})
-})
+	it('Should has a UsersList Component', () => {
+		const usersListComponent = wrapper.findComponent(UsersList);
+		expect(usersListComponent.exists()).toBeTruthy();
+	});
+
+	it('Should has a Configuration Form Component', () => {
+		const configuration = wrapper.findComponent(Configuration);
+		expect(configuration.exists()).toBeTruthy();
+	});
+});
