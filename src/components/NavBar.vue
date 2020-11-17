01334e014ae1da88829c9bba638deb322f7a8f47
Andres Ruiz <andrsruiz@gmail.com>
2020-11-17 17:13:01 -0600

    Set Prop of SideBar in NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,15 +6,15 @@ div.navbar.has-background-primary-dark
 		icon-pack='fas'
 		icon-left='bars'
 		@click='changeSideBarState()'
-		v-if='user'
 	) {{ menu }}
-	SideBar( v-if='user' )
 	h1.is-size-3.has-text-primary-white(
 		title='Title'
 	) {{ title }}
+	SideBar( :isMobile='mobile' )
 </template>
 
 <script lang='js'>
+import { EventBus } from '@/eventBus.js'
 import SideBar from '@/components/SideBar.vue'
 
 export default {
@@ -23,22 +23,32 @@ export default {
 	data() {
 		return {
 			title: 'Tyra Web',
-			menu: 'Menu'
-		}
-	},
-	methods: {
-		changeSideBarState(){
-			this.$store.commit('changeSideBarState')
+			menu: 'Menu',
+			mobile: true
 		}
 	},
 	computed: {
-		user(){
-			return this.$store.state.user
-		}
+		config(){ return this.$store.state.config }
+	},
+	methods: {
+		init(){ if(this.config) this.title = this.config.vetName },
+		changeSideBarState(){ this.$store.commit('changeSideBarState') }
+	},
+	created(){
+		/* Event Listeners
+		 *
+		 * Listens when the config object is updated. Then
+		 * it updates the app title. */
+		EventBus.$on('update-config', () => {
+			this.title = this.config.vetName
+		})
+	},
+	mounted(){
+		this.init()
 	}
 }
 </script>
 
-<style>
+<style scoped>
 @import '../assets/css/navbar.css'
 </style>
